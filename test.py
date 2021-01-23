4cb2ff68922ad2cf8b0fe28a5cd6882f2c055fa9
MinChul-Son <smc5236@naver.com>
2021-01-23 20:34:36 +0900

    21/1/23 algorithm_study3

--- test.py
+++ test.py
@@ -49,3 +49,33 @@
 # print(solution(4,[3,2,3,1]))
 
 
+#배열을 연결리스트로 표현한 것과 비슷
+#하나씩 뽑아서 값을 보고 인덱스를 쫓아들어감. 그 떄마다 count +1 해서 나중에 max값
+# from operator import itemgetter, attrgetter
+
+# def solution(next_student):
+#     count_list = []
+#     for i,p in enumerate(next_student):
+#         if p == 0 :
+#             count_list.append((i+1,1))
+#         else:
+#             student_num = p
+#             count = 1
+#             temp_list = [i+1]
+#             while True:
+#                 if next_student[student_num-1] != 0:
+#                     if student_num not in temp_list:
+#                         count += 1
+#                         temp_list.append(student_num)
+#                         student_num = next_student[student_num-1]
+#                     else:
+#                         count_list.append((count,i+1))
+#                         break
+#                 else:
+#                     count += 1
+#                     count_list.append((count,i+1))
+#                     break
+#     answer = sorted(count_list, key=itemgetter(0,1))
+#     return answer[-1][1]
+
+# print(solution([5,9,13,1,0,0,11,1,7,12,9,9,2]))
