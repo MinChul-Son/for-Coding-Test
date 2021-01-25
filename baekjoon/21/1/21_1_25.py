8bbf879cc50900ebdf4ebb0e7ce69488e90f13bf
MinChul-Son <smc5236@naver.com>
2021-01-25 18:49:14 +0900

    21/1/25 algorithm_study2

--- baekjoon/21/1/21_1_25.py
+++ baekjoon/21/1/21_1_25.py
@@ -8,3 +8,15 @@
 #     if sum(i) <= m and answer < sum(i):
 #         answer = sum(i)
 # print(answer)
+
+# https://www.acmicpc.net/problem/2231
+# n = int(input())
+# answer = 0
+# for i in range(1,n+1):
+#     temp = list(map(int,str(i)))
+#     answer = i + sum(temp)
+#     if answer == n:
+#         print(i)
+#         break
+#     if i == n:
+#         print(0)
