f10239af56ca38b4e8bace36120397cc7607a669
MinChul-Son <smc5236@naver.com>
2021-01-25 18:33:24 +0900

    21/1/25 algorithm_study

---
+++ baekjoon/21/1/21_1_25.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/2798
+# from itertools import combinations
+# n, m = map(int,input().split())
+# card_list = list(map(int,input().split()))
+# combi = list(combinations(card_list,3))
+# answer = 0
+# for i in combi:
+#     if sum(i) <= m and answer < sum(i):
+#         answer = sum(i)
+# print(answer)
