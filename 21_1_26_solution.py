55c6aed062ef16613ddd51b99f9720e582d2ae60
MinChul-Son <smc5236@naver.com>
2021-01-26 17:57:46 +0900

    21/1/26 algorithm_study

---
+++ 21_1_26_solution.py
@@ -0,0 +1,25 @@
+#동빈나 구현 알고리즘 강의
+
+
+# n = int(input())
+# plan_list = list(input().split())
+# start_point =[1,1]
+
+# for i in plan_list:
+#     if i == 'L':
+#         if start_point[1] == 1:
+#             continue
+#         start_point[1] -= 1
+#     elif i == 'R':
+#         if start_point[1] == n:
+#             continue
+#         start_point[1] += 1
+#     elif i == 'U':
+#         if start_point[0] == 1:
+#             continue
+#         start_point[0] -= 1
+#     else:
+#         if start_point[0] == n:
+#             continue
+#         start_point[0] += 1
+# print(" ".join(map(str,start_point)))
--- programmers/21_1_20_solution.py
+++ programmers/21_1_20_solution.py
@@ -30,17 +30,17 @@
 
 # https://programmers.co.kr/learn/courses/30/lessons/12949?language=python3
 
-# def solution(arr1, arr2):
-#     answer = []
-#         for idx1 in range(len(arr1)):
-#         row = []
-#         for idx2 in range(len(arr2[0])):
-#             tmp = 0
-#             for idx3 in range(len(arr1[0])):
-#                 tmp += arr1[idx1][idx3] * arr2[idx3][idx2]
-#             row.append(tmp)
-#         answer.append(row)
-#     return answer
+    # def solution(arr1, arr2):
+    #     answer = []
+    #         for idx1 in range(len(arr1)):
+    #         row = []
+    #         for idx2 in range(len(arr2[0])):
+    #             tmp = 0
+    #             for idx3 in range(len(arr1[0])):
+    #                 tmp += arr1[idx1][idx3] * arr2[idx3][idx2]
+    #             row.append(tmp)
+    #         answer.append(row)
+    #     return answer
 
 
 
