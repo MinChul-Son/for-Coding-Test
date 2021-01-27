a998e0f89c1723ab0c2755a60b9f902ce1372637
MinChul-Son <smc5236@naver.com>
2021-01-27 15:22:20 +0900

    21/1/27 algorithm_study 2

--- baekjoon/21/1/21_1_27.py
+++ baekjoon/21/1/21_1_27.py
@@ -15,5 +15,48 @@
 
 # https://www.acmicpc.net/problem/1018
 
+# n,m = map(int,input().split())
+# board = []
+# for i in range(n):
+#     wb = list(map(str,input()))
+#     board.append(wb)
+
+# white_board = [['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W']]
+# black_board = [['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B'], 
+#             ['B','W','B','W','B','W','B','W'], 
+#             ['W','B','W','B','W','B','W','B']]
+# result = []
+
+# for j in range(m-7):
+#     q = 0
+#     w = 0
+#     for k in range(n-7):
+#         count_black = 0
+#         count_white = 0
+#         for l in range(j,j+8):
+#             for x in range(k,k+8):
+#                 if board[x][l] != white_board[q][w]:
+#                     count_white +=1
+#                 if board[x][l] != black_board[q][w]:
+#                     count_black +=1
+#                 w+=1
+#             w = 0
+#             q+=1
+#         q = 0
+#         result.append(count_black)
+#         result.append(count_white)
+# print(min(result))
 
 
