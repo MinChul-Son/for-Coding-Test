e2c16b9879071eabe4ba98a18558eb40a86272b2
MinChul-Son <smc5236@naver.com>
2021-01-31 17:44:26 +0900

    21/1/31 bfs_dfs_study

--- baekjoon/21/1/21_1_30.py
+++ baekjoon/21/1/21_1_30.py
@@ -177,19 +177,19 @@
 #         print(-1,end=" ")
 
 
-num = int(input())
-a = list(map(int, input().split(" ")))
-result = ["-1" for _ in range(num)]
-stack = []
-stack.append(0)
-i = 1
-for i in range(num):
-    while stack and a[stack[-1]] < a[i]:
-        result[stack[-1]] = str(a[i])
-        stack.pop()
+# num = int(input())
+# a = list(map(int, input().split(" ")))
+# result = ["-1" for _ in range(num)]
+# stack = []
+# stack.append(0)
+# i = 1
+# for i in range(num):
+#     while stack and a[stack[-1]] < a[i]:
+#         result[stack[-1]] = str(a[i])
+#         stack.pop()
 
-    stack.append(i)
-    i += 1
+#     stack.append(i)
+#     i += 1
 
-print(" ".join(result))
+# print(" ".join(result))
 
