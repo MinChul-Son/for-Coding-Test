f9f21671420411387a69b550482faf46c1d4673f
MinChul-Son <smc5236@naver.com>
2021-02-03 20:37:52 +0900

    21/2/3 algorithm study2

--- baekjoon/21/1/21_2_3.py
+++ baekjoon/21/1/21_2_3.py
@@ -1,16 +1,56 @@
 # https://www.acmicpc.net/problem/1003
 
-n = int(input())
-count0=[1,0,1]
-count1=[0,1,1]
+# n = int(input())
+# count0=[1,0,1]
+# count1=[0,1,1]
 
-def fibonacci (k):
-    if len(count0) <= k:
-        for i in range(len(count0), k+1):
-            count0.append(count0[i-1]+count0[i-2])
-            count1.append(count1[i-1]+count1[i-2])
-    print(count0[k],count1[k])
+# def fibonacci (k):
+#     if len(count0) <= k:
+#         for i in range(len(count0), k+1):
+#             count0.append(count0[i-1]+count0[i-2])
+#             count1.append(count1[i-1]+count1[i-2])
+#     print(count0[k],count1[k])
 
-for _ in range(n):
-    N = int(input())
-    fibonacci(N)
+# for _ in range(n):
+#     N = int(input())
+#     fibonacci(N)
+
+# https://www.acmicpc.net/problem/2438
+# n = int(input())
+# for i in range(1,n+1):
+#     temp = '*' * i
+#     print(temp)
+
+# https://www.acmicpc.net/problem/11720
+# n = int(input())
+# N = list(map(int,list(input())))
+# print(sum(N))
+
+# https://www.acmicpc.net/problem/2577
+# a= int(input())
+# b= int(input())
+# c= int(input())
+# temp = str(a*b*c)
+# for i in range(10):
+#     print(temp.count(str(i)))
+
+# https://www.acmicpc.net/problem/8958
+
+# n = int(input())
+# for _ in range(n):
+#     test = input()
+#     score = 0
+#     temp = 0
+#     for i in test:
+#         if i == 'O':
+#             temp += 1
+#             score += temp
+#         else:
+#             temp = 0
+#     print(score)
+
+
+# https://www.acmicpc.net/problem/1152
+# n = input().split()
+ 
+# print(len(n))
