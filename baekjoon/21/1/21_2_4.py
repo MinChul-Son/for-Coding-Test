127a304299e743f0bb09ba18a8149840ccf0202b
MinChul-Son <smc5236@naver.com>
2021-02-04 16:20:27 +0900

    21/2/4 algorithm_study

---
+++ baekjoon/21/1/21_2_4.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1010
+# import math
+# t = int(input())
+
+# for _ in range(t):
+#     n, m = map(int,input().split())
+#     temp = math.factorial(m-n)
+#     m = math.factorial(m)
+#     n = math.factorial(n)
+#     print(m//(n*temp))
