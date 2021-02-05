8b64652240614fb65548534a1d4e1a19400339b3
MinChul-Son <smc5236@naver.com>
2021-02-05 17:31:05 +0900

    21/2/5 algorithm_study2

--- baekjoon/21/1/21_2_5.py
+++ baekjoon/21/1/21_2_5.py
@@ -1,12 +1,26 @@
 # https://www.acmicpc.net/problem/1463
-n = int(input())
-dp = [0 for _ in range(n+1)]
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
+# n = int(input())
+# dp = [0 for _ in range(n+1)]
+# for i in range(2, n+1):
+#     dp[i] = dp[i-1] + 1
 
-    if i % 2 == 0 and dp[i] > dp[i//2] +1:
-        dp[i] = dp[i//2] + 1
-    if i % 3 == 0 and dp[i] > dp[i//3] +1:
-        dp[i] = dp[i//3] + 1
-print(dp[n])
-print(dp)
+#     if i % 2 == 0 and dp[i] > dp[i//2] +1:
+#         dp[i] = dp[i//2] + 1
+#     if i % 3 == 0 and dp[i] > dp[i//3] +1:
+#         dp[i] = dp[i//3] + 1
+# print(dp[n])
+# print(dp)
+
+# https://www.acmicpc.net/problem/9095
+
+t = int(input())
+for _ in range(t):
+    dp = [1,2,4]
+    n = int(input())
+    if n <= 3:
+        print(dp[n-1])
+    else:
+        for i in range(3,n):
+            dp.append((dp[i-3]+dp[i-2]+dp[i-1]))
+        print(dp[n-1])
+
