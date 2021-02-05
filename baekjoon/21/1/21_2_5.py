daca4b85f79d98cf9b185f8496e9013aa2e031e8
MinChul-Son <smc5236@naver.com>
2021-02-05 18:01:19 +0900

    21/2/5 algorithm_study3

--- baekjoon/21/1/21_2_5.py
+++ baekjoon/21/1/21_2_5.py
@@ -13,14 +13,39 @@
 
 # https://www.acmicpc.net/problem/9095
 
-t = int(input())
-for _ in range(t):
-    dp = [1,2,4]
-    n = int(input())
-    if n <= 3:
-        print(dp[n-1])
+# t = int(input())
+# for _ in range(t):
+#     dp = [1,2,4]
+#     n = int(input())
+#     if n <= 3:
+#         print(dp[n-1])
+#     else:
+#         for i in range(3,n):
+#             dp.append((dp[i-3]+dp[i-2]+dp[i-1]))
+#         print(dp[n-1])
+
+
+
+# https://www.acmicpc.net/problem/2579
+import sys
+n = int(input())
+N = [int(sys.stdin.readline().strip()) for _ in range(n)]
+dp = []
+if n <=3:
+    if n == 1:
+        dp.append(N[0])
+    elif n == 2:
+        dp.append(sum(N))
     else:
-        for i in range(3,n):
-            dp.append((dp[i-3]+dp[i-2]+dp[i-1]))
-        print(dp[n-1])
+        dp.append(N[0]) 
+        dp.append(max(N[0]+N[1],N[1])) 
+        dp.append(max(N[0]+N[2],N[1]+N[2]))
+    print(dp[-1]) 
+else:
+    dp.append(N[0]) 
+    dp.append(max(N[0]+N[1],N[1])) 
+    dp.append(max(N[0]+N[2],N[1]+N[2])) 
+    for i in range(3,n): 
+        dp.append(max(dp[i-2] + N[i] , dp[i-3] + N[i] + N[i - 1]))
+    print(dp[-1])
 
