433ffa009f77304aee628d9aea30de2b1d516850
MinChul-Son <smc5236@naver.com>
2021-02-06 21:42:52 +0900

    21/2/6 algorithm_study3

--- baekjoon/21/1/21_2_6.py
+++ baekjoon/21/1/21_2_6.py
@@ -35,115 +35,142 @@
 
 
 # https://www.acmicpc.net/problem/1063
-import sys
-k, s, n = input().split()
-move = [sys.stdin.readline().strip() for _ in range(int(n))]
-row = ['A','B','C','D','E','F','G','H']
-column = ['1','2','3','4','5','6','7','8']
-king_idx = []
-stone_idx = []
-for i,p in enumerate(row):
-    if p == k[0]:
-        king_idx.append(i)
-        break
-for i,p in enumerate(column):
-    if p == k[1]:
-        king_idx.append(i)
-        break
-for i,p in enumerate(row):
-    if p == s[0]:
-        stone_idx.append(i)
-        break
-for i,p in enumerate(column):
-    if p == s[1]:
-        stone_idx.append(i)
-        break
+# import sys
+# k, s, n = input().split()
+# move = [sys.stdin.readline().strip() for _ in range(int(n))]
+# row = ['A','B','C','D','E','F','G','H']
+# column = ['1','2','3','4','5','6','7','8']
+# king_idx = []
+# stone_idx = []
+# for i,p in enumerate(row):
+#     if p == k[0]:
+#         king_idx.append(i)
+#         break
+# for i,p in enumerate(column):
+#     if p == k[1]:
+#         king_idx.append(i)
+#         break
+# for i,p in enumerate(row):
+#     if p == s[0]:
+#         stone_idx.append(i)
+#         break
+# for i,p in enumerate(column):
+#     if p == s[1]:
+#         stone_idx.append(i)
+#         break
 
-# 명령 수행
-for i in move:
-    if i == 'R':
-        if king_idx[0] == 7:
-            continue
-        king_idx[0] += 1
-        if king_idx == stone_idx:
-            if stone_idx[0] == 7:
-                king_idx[0] -= 1
-                continue
-            stone_idx[0] += 1
-    elif i == 'L':
-        if king_idx[0] == 0:
-            continue
-        king_idx[0] -= 1
-        if king_idx == stone_idx:
-            if stone_idx[0] == 0:
-                king_idx[0] += 1
-                continue
-            stone_idx[0] -= 1
-    elif i == 'B':
-        if king_idx[1] == 0:
-            continue
-        king_idx[1] -= 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 0:
-                king_idx[1] += 1
-                continue
-            stone_idx[1] -= 1
-    elif i == 'T':
-        if king_idx[1] == 7:
-            continue
-        king_idx[1] += 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 7:
-                king_idx[1] -= 1
-                continue
-            stone_idx[1] += 1
-    elif i == 'RT':
-        if king_idx[1] == 7 or king_idx[0] == 7:
-            continue
-        king_idx[0] += 1
-        king_idx[1] += 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 7 or stone_idx[0] == 7:
-                king_idx[0] -= 1
-                king_idx[1] -= 1
-                continue
-            stone_idx[0] += 1
-            stone_idx[1] += 1
-    elif i == 'LT':
-        if king_idx[1] == 7 or king_idx[0] == 0:
-            continue
-        king_idx[0] -= 1
-        king_idx[1] += 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 7 or stone_idx[0] == 0:
-                king_idx[0] += 1
-                king_idx[1] -= 1
-                continue
-            stone_idx[0] -= 1
-            stone_idx[1] += 1
-    elif i == 'RB':
-        if king_idx[1] == 0 or king_idx[0] == 7:
-            continue
-        king_idx[0] += 1
-        king_idx[1] -= 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 0 or stone_idx[0] == 7:
-                king_idx[0] -= 1
-                king_idx[1] += 1
-                continue
-            stone_idx[0] += 1
-            stone_idx[1] -= 1
-    elif i == 'LB':
-        if king_idx[1] == 0 or king_idx[0] == 0:
-            continue
-        king_idx[0] -= 1
-        king_idx[1] -= 1
-        if king_idx == stone_idx:
-            if stone_idx[1] == 0 or stone_idx[0] == 0:
-                king_idx[0] += 1
-                king_idx[1] += 1
-                continue
-            stone_idx[0] -= 1
-            stone_idx[1] -= 1
-print(row[king_idx[0]]+column[king_idx[1]])
-print(row[stone_idx[0]]+column[stone_idx[1]])
+# # 명령 수행
+# for i in move:
+#     if i == 'R':
+#         if king_idx[0] == 7:
+#             continue
+#         king_idx[0] += 1
+#         if king_idx == stone_idx:
+#             if stone_idx[0] == 7:
+#                 king_idx[0] -= 1
+#                 continue
+#             stone_idx[0] += 1
+#     elif i == 'L':
+#         if king_idx[0] == 0:
+#             continue
+#         king_idx[0] -= 1
+#         if king_idx == stone_idx:
+#             if stone_idx[0] == 0:
+#                 king_idx[0] += 1
+#                 continue
+#             stone_idx[0] -= 1
+#     elif i == 'B':
+#         if king_idx[1] == 0:
+#             continue
+#         king_idx[1] -= 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 0:
+#                 king_idx[1] += 1
+#                 continue
+#             stone_idx[1] -= 1
+#     elif i == 'T':
+#         if king_idx[1] == 7:
+#             continue
+#         king_idx[1] += 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 7:
+#                 king_idx[1] -= 1
+#                 continue
+#             stone_idx[1] += 1
+#     elif i == 'RT':
+#         if king_idx[1] == 7 or king_idx[0] == 7:
+#             continue
+#         king_idx[0] += 1
+#         king_idx[1] += 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 7 or stone_idx[0] == 7:
+#                 king_idx[0] -= 1
+#                 king_idx[1] -= 1
+#                 continue
+#             stone_idx[0] += 1
+#             stone_idx[1] += 1
+#     elif i == 'LT':
+#         if king_idx[1] == 7 or king_idx[0] == 0:
+#             continue
+#         king_idx[0] -= 1
+#         king_idx[1] += 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 7 or stone_idx[0] == 0:
+#                 king_idx[0] += 1
+#                 king_idx[1] -= 1
+#                 continue
+#             stone_idx[0] -= 1
+#             stone_idx[1] += 1
+#     elif i == 'RB':
+#         if king_idx[1] == 0 or king_idx[0] == 7:
+#             continue
+#         king_idx[0] += 1
+#         king_idx[1] -= 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 0 or stone_idx[0] == 7:
+#                 king_idx[0] -= 1
+#                 king_idx[1] += 1
+#                 continue
+#             stone_idx[0] += 1
+#             stone_idx[1] -= 1
+#     elif i == 'LB':
+#         if king_idx[1] == 0 or king_idx[0] == 0:
+#             continue
+#         king_idx[0] -= 1
+#         king_idx[1] -= 1
+#         if king_idx == stone_idx:
+#             if stone_idx[1] == 0 or stone_idx[0] == 0:
+#                 king_idx[0] += 1
+#                 king_idx[1] += 1
+#                 continue
+#             stone_idx[0] -= 1
+#             stone_idx[1] -= 1
+# print(row[king_idx[0]]+column[king_idx[1]])
+# print(row[stone_idx[0]]+column[stone_idx[1]])
+
+
+# https://www.acmicpc.net/problem/1934
+# from math import gcd
+# n = int(input())
+# for _ in range(n):
+#     n, m = map(int,input().split())
+#     print((n*m)//gcd(n,m))
+
+# https://www.acmicpc.net/problem/1476
+# e, s, m = map(int,input().split())
+# E, S, M, answer = 1, 1, 1, 1
+# while True:
+#     if e == E and s == S and m == M:
+#         break
+#     E += 1
+#     S += 1
+#     M += 1
+#     answer += 1
+#     if E >= 16:
+#         E -= 15
+#     if S >= 29:
+#         S -= 28
+#     if M >= 20:
+#         M -= 19
+# print(answer)
+
