be15fed678472fdb080d4af369240d39695f8f16
MinChul-Son <smc5236@naver.com>
2021-02-07 14:14:35 +0900

    21/2/7 algorithm_study3

--- baekjoon/21/1/21_2_7.py
+++ baekjoon/21/1/21_2_7.py
@@ -20,13 +20,53 @@
 # print(A[k-1])
 
 # https://www.acmicpc.net/problem/10610
-n = list(input())
-n.sort(reverse=True)
-print(n)
-answer =  -1
-max = 0
-if n[-1] == '0':
-    max = int("".join(n))
-    if max % 3 == 0:
-        answer = max
-print(answer)
+# n = list(input())
+# n.sort(reverse=True)
+# answer =  -1
+# max = 0
+# if n[-1] == '0':
+#     max = int("".join(n))
+#     if max % 3 == 0:
+#         answer = max
+# print(answer)
+
+# https://www.acmicpc.net/problem/10867
+# n = int(input())
+# N = list(map(int,set(input().split())))
+# for i in sorted(N):
+#     print(i, end=" ")
+
+# https://www.acmicpc.net/problem/1978
+# n = int(input())
+# N = list(map(int, input().split()))
+# answer = 0
+# for i in N:
+#     count = 0
+#     if i != 1:
+#         for j in range(1,i+1):
+#             if i % j == 0:
+#                 count += 1
+#         if count == 2:
+#             answer += 1
+# print(answer)
+
+# https://www.acmicpc.net/problem/2581
+m = int(input())
+n = int(input())
+temp_list = []
+for i in range(m, n+1):
+    count = 0
+    if i != 1:
+        for j in range(1, i+1):
+            if i % j == 0:
+                count += 1
+        if count == 2:
+            temp_list.append(i)
+if temp_list:
+    print(sum(temp_list))
+    print(min(temp_list))
+else:
+    print(-1)
+
+
+
