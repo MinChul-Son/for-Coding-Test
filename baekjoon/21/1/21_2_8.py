2a5f0b34c784696ff912bcd4c2e715f19f93c17b
MinChul-Son <smc5236@naver.com>
2021-02-08 15:07:19 +0900

    21/2/8 algorithm_study

--- baekjoon/21/1/21_2_8.py
+++ baekjoon/21/1/21_2_8.py
@@ -1,14 +1,27 @@
 # https://www.acmicpc.net/problem/1110
-N = int(input())
-cycle = 0
-new_num = N
-while True:
-    temp = str(new_num)
-    cycle += 1
-    if int(temp) < 10:
-        new_num = int(temp + temp)
-    else:
-        new_num = int(temp[-1] + str(int(temp[0]) + int(temp[1]))[-1])
-    if new_num == N:
+# N = int(input())
+# cycle = 0
+# new_num = N
+# while True:
+#     temp = str(new_num)
+#     cycle += 1
+#     if int(temp) < 10:
+#         new_num = int(temp + temp)
+#     else:
+#         new_num = int(temp[-1] + str(int(temp[0]) + int(temp[1]))[-1])
+#     if new_num == N:
+#         break
+# print(cycle)
+
+# https://www.acmicpc.net/problem/2839
+n = int(input())
+count = 0
+while n >= 0:
+    if n % 5 == 0:
+        count += (n//5)
+        print(count)
         break
-print(cycle)
+    n -= 3
+    count += 1
+else:
+    print(-1)
