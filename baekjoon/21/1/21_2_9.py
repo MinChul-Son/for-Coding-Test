7721d5f0c5b3b5d136da90ea967f6e60eaa99322
MinChul-Son <smc5236@naver.com>
2021-02-09 17:35:06 +0900

    21/2/9 algorithm_study2

--- baekjoon/21/1/21_2_9.py
+++ baekjoon/21/1/21_2_9.py
@@ -1,7 +1,20 @@
 # https://www.acmicpc.net/problem/2908
-a, b = input().split()
-a = list(a); b = list(b); a.reverse(); b.reverse(); a = int("".join(a)); b = int("".join(b))
-if a > b:
-    print(a)
-else:
-    print(b)
+# a, b = input().split()
+# a = list(a); b = list(b); a.reverse(); b.reverse(); a = int("".join(a)); b = int("".join(b))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# https://www.acmicpc.net/problem/10809
+# s = input()
+# for i in range(97, 123):
+#     if chr(i) in s:
+#         print(s.index(chr(i)),end=" ")
+#     else:
+#         print(-1,end=" ")
+
+# https://www.acmicpc.net/problem/11654
+# print(ord(input()))
+
+# https://www.acmicpc.net/problem/2675
