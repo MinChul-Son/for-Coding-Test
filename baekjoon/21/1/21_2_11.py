19c7187c0dd0f5157bf6548edc08adc9d5b3e66d
MinChul-Son <smc5236@naver.com>
2021-02-11 17:03:48 +0900

    21/2/11 algorithm_study2

--- baekjoon/21/1/21_2_11.py
+++ baekjoon/21/1/21_2_11.py
@@ -10,3 +10,16 @@
 # print(factorial(n))
 
 
+# https://www.acmicpc.net/problem/10870
+# def fibonacci(num):
+#     if num <= 0:
+#         return 0
+#     elif num == 1:
+#         return 1
+#     else:
+#         return fibonacci(num-1) + fibonacci(num-2)
+# n = int(input())
+# print(fibonacci(n))
+
+
+# https://www.acmicpc.net/problem/2447
