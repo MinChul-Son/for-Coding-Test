ec12e36999f2bafab500897b15192acf68db2749
MinChul-Son <smc5236@naver.com>
2021-02-11 16:59:45 +0900

    21/2/11 algorithm_study1

---
+++ baekjoon/21/1/21_2_11.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/10872
+# def factorial(num):
+#     if num == 0 or num == 1:
+#         return 1
+#     else:
+#         return num*factorial(num-1)
+
+
+# n = int(input())
+# print(factorial(n))
+
+
