2c34d5ab4916db8a065271ab1a9a0e5cf9952e7c
MinChul-Son <smc5236@naver.com>
2021-02-12 20:46:46 +0900

    21/2/12 algorithm_study1

--- baekjoon/21/1/21_2_12.py
+++ baekjoon/21/1/21_2_12.py
@@ -1,23 +1,39 @@
 # https://www.acmicpc.net/problem/2981
-import sys 
-import math 
-N = int(sys.stdin.readline()) 
-nums = [] 
-for i in range(N): 
-    nums.append(int(sys.stdin.readline())) 
-    nums.sort() 
-    mog = nums[-1] - nums[0] 
-    divisor = [mog] 
-    for i in range(2, int(math.sqrt(mog)) + 1): 
-        if mog % i == 0: 
-            divisor.append(i) 
-            divisor.append(mog//i) 
-divisor = list(set(divisor)) 
-divisor.sort() 
-for d in divisor: 
-    for i in range(N): 
-        if i == N - 1: 
-            print(d, end = " ") 
-        elif nums[i] % d != nums[i + 1] % d: 
-            break
+# import sys 
+# import math 
+# N = int(sys.stdin.readline()) 
+# nums = [] 
+# for i in range(N): 
+#     nums.append(int(sys.stdin.readline())) 
+#     nums.sort() 
+#     mog = nums[-1] - nums[0] 
+#     divisor = [mog] 
+#     for i in range(2, int(math.sqrt(mog)) + 1): 
+#         if mog % i == 0: 
+#             divisor.append(i) 
+#             divisor.append(mog//i) 
+# divisor = list(set(divisor)) 
+# divisor.sort() 
+# for d in divisor: 
+#     for i in range(N): 
+#         if i == N - 1: 
+#             print(d, end = " ") 
+#         elif nums[i] % d != nums[i + 1] % d: 
+#             break
+
+# https://www.acmicpc.net/problem/5086
+# while True:
+#     n, m = map(int, input().split())
+#     if n == 0 and m == 0:
+#         break
+#     if n % m == 0:
+#         print("multiple")
+#     else:
+#         if m % n == 0:
+#             print("factor")
+#         else:
+#             print("neither")
+
+
+
 
