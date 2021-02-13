9c30f1c07ec3ae8db49a58ace2b8b18f8782d572
MinChul-Son <smc5236@naver.com>
2021-02-13 21:37:35 +0900

    21/2/13 algorithm_study1

---
+++ baekjoon/21/1/21_2_13.py
@@ -0,0 +1,5 @@
+# https://www.acmicpc.net/problem/11051
+# from math import factorial
+# n, k = map(int, input().split())
+# result = factorial(n) // (factorial(k) * factorial(n - k))
+# print(result % 10007)
