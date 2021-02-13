55f767ae66026c1192beda399893661624fe8d95
MinChul-Son <smc5236@naver.com>
2021-02-13 22:26:38 +0900

    21/2/13 algorithm_study3

--- baekjoon/21/1/21_2_13.py
+++ baekjoon/21/1/21_2_13.py
@@ -5,25 +5,38 @@
 # print(result % 10007)
 
 # https://www.acmicpc.net/problem/9375
-import sys
-t = int(input())
-for _ in range(t):
-    answer = 1
-    kinds = []
-    n = int(input())
-    cloth = [sys.stdin.readline().split() for _ in range(n)]
-    for i in cloth:
-        count = 0
-        temp = []
-        for j in range(len(cloth)):
-            if i[1] == cloth[j][1]:
-                count += 1
-        temp.append(count)
-        temp.append(i[1])
-        if kinds.count(temp) == 0:
-            kinds.append(temp)
-            answer = answer * (temp[0]+1)
-        else:
-            pass
-    answer -= 1
-    print(answer)
+# import sys
+# t = int(input())
+# for _ in range(t):
+#     answer = 1
+#     kinds = []
+#     n = int(input())
+#     cloth = [sys.stdin.readline().split() for _ in range(n)]
+#     for i in cloth:
+#         count = 0
+#         temp = []
+#         for j in range(len(cloth)):
+#             if i[1] == cloth[j][1]:
+#                 count += 1
+#         temp.append(count)
+#         temp.append(i[1])
+#         if kinds.count(temp) == 0:
+#             kinds.append(temp)
+#             answer = answer * (temp[0]+1)
+#         else:
+#             pass
+#     answer -= 1
+#     print(answer)
+
+# https://www.acmicpc.net/problem/1676
+from math import factorial
+n = int(input())
+fac = list(str(factorial(n)))
+fac.reverse()
+count = 0
+for i in fac:
+    if i == '0':
+        count += 1
+    else:
+        break
+print(count)
