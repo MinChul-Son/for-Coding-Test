d09db5c0343a59e40cfd4b498a03505f6da8c462
MinChul-Son <smc5236@naver.com>
2021-02-13 22:11:43 +0900

    21/2/13 algorithm_study2

--- baekjoon/21/1/21_2_13.py
+++ baekjoon/21/1/21_2_13.py
@@ -3,3 +3,27 @@
 # n, k = map(int, input().split())
 # result = factorial(n) // (factorial(k) * factorial(n - k))
 # print(result % 10007)
+
+# https://www.acmicpc.net/problem/9375
+import sys
+t = int(input())
+for _ in range(t):
+    answer = 1
+    kinds = []
+    n = int(input())
+    cloth = [sys.stdin.readline().split() for _ in range(n)]
+    for i in cloth:
+        count = 0
+        temp = []
+        for j in range(len(cloth)):
+            if i[1] == cloth[j][1]:
+                count += 1
+        temp.append(count)
+        temp.append(i[1])
+        if kinds.count(temp) == 0:
+            kinds.append(temp)
+            answer = answer * (temp[0]+1)
+        else:
+            pass
+    answer -= 1
+    print(answer)
