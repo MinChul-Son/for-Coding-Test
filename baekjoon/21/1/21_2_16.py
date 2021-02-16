ae7a684ea8a936405facf7ed5b4ff16d4c74e2d8
MinChul-Son <smc5236@naver.com>
2021-02-16 14:54:01 +0900

    21/2/16 algorithm_study3

--- baekjoon/21/1/21_2_16.py
+++ baekjoon/21/1/21_2_16.py
@@ -1,12 +1,26 @@
 # https://www.acmicpc.net/problem/1712
-# a, b, c = map(int, input().split())
-# count = 1
-# if b >= c:
-#     print(-1)
+# A, B, C = list(map(int, input().split()))
+# BREAK_EVEN_POINT = 0
+
+# if(C <= B):
+#     BREAK_EVEN_POINT = -1
 # else:
-#     while True:
-#         cost = a + (b*count)
-#         if cost < c*count:
-#             break
-#         count += 1
-#     print(count)
+#     BREAK_EVEN_POINT = A // (C - B) + 1 
+# print(BREAK_EVEN_POINT)
+
+
+# https://www.acmicpc.net/problem/2292
+# n = int(input())
+# cnt = 1
+# cnt_six = 6
+# count = 1
+# while n > cnt:
+#     count += 1
+#     cnt += cnt_six
+#     cnt_six += 6
+# print(count)
+
+
+# https://www.acmicpc.net/problem/13305
+
+
