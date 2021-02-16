a2ce5129ddb59e329f2c9afa36148092fe60562d
MinChul-Son <smc5236@naver.com>
2021-02-16 15:27:39 +0900

    21/2/16 algorithm_study4

--- baekjoon/21/1/21_2_16.py
+++ baekjoon/21/1/21_2_16.py
@@ -22,5 +22,31 @@
 
 
 # https://www.acmicpc.net/problem/13305
+# n = int(input())
+# path = list(map(int, input().split()))
+# cost = list(map(int, input().split()))
+# sum_cost = 0
+# # 나보다 오른쪽 도시의 기름값이 더 싼곳(맨 끝값은 제외)이 있다면 거리만큼만 기름 충전, 없다면 남은 거리 싹다 충전해버리기
+# temp = cost[0]
+# for i in range(n-1):
+#     if cost[i] <= temp:
+#         temp = cost[i]
+#     sum_cost += temp*path[i]
+# print(sum_cost)
+
+
+# https://www.acmicpc.net/problem/11721
+# n = input()
+# for i,p in enumerate(n):
+#     print(p, end="")
+#     if str(i)[-1] == '9':
+#         print("")
+
+# https://www.acmicpc.net/problem/11719
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
 
 
