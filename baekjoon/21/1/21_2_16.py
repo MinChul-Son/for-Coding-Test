afca52d412eeac9989204570f4c216a33d3d9d52
MinChul-Son <smc5236@naver.com>
2021-02-16 14:43:14 +0900

    21/2/16 algorithm_study1

---
+++ baekjoon/21/1/21_2_16.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1712
+# a, b, c = map(int, input().split())
+# count = 1
+# if b >= c:
+#     print(-1)
+# else:
+#     while True:
+#         cost = a + (b*count)
+#         if cost < c*count:
+#             break
+#         count += 1
+#     print(count)
