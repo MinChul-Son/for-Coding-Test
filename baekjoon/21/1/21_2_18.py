406374d3f5bfaf3f745ddd4ec337b8f60bcef646
MinChul-Son <smc5236@naver.com>
2021-02-18 17:16:12 +0900

    21/2/18 algorithm_study

--- baekjoon/21/1/21_2_18.py
+++ baekjoon/21/1/21_2_18.py
@@ -1,8 +1,22 @@
 # https://www.acmicpc.net/problem/1026
+# n = int(input())
+# a = sorted(list(map(int, input().split())))
+# b = sorted(list(map(int, input().split())), reverse=True)
+# sum = 0
+# for i in range(n):
+#     sum += (a[i]*b[i])
+# print(sum)
+
+
+# https://www.acmicpc.net/problem/10815
+from bisect import bisect_left, bisect_right
 n = int(input())
-a = sorted(list(map(int, input().split())))
-b = sorted(list(map(int, input().split())), reverse=True)
-sum = 0
-for i in range(n):
-    sum += (a[i]*b[i])
-print(sum)
+my_card = sorted(list(map(int, input().split())))
+m = int(input())
+input_card = list(map(int, input().split()))
+for i in input_card:
+    if bisect_left(my_card, i) == bisect_right(my_card, i):
+        print(0, end=" ")
+    else:
+        print(1, end=" ")
+
