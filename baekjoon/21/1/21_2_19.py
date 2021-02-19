657faf524bb25e95e8a22a39e2d3579010fc2291
MinChul-Son <smc5236@naver.com>
2021-02-19 15:45:52 +0900

    21/2/19 algorithm_study2

--- baekjoon/21/1/21_2_19.py
+++ baekjoon/21/1/21_2_19.py
@@ -1,16 +1,69 @@
 # https://www.acmicpc.net/problem/1927
-import heapq
-import sys
-n = int(input())
-queue = []
-for _ in range(n):
-    x = int(sys.stdin.readline())
-    if x != 0:
-        heapq.heappush(queue,x)
-    else:
-        if not queue:
-            print(0)
-        else:
-            print(heapq.heappop(queue))
-    
+# import heapq
+# import sys
+# n = int(input())
+# queue = []
+# for _ in range(n):
+#     x = int(sys.stdin.readline())
+#     if x != 0:
+#         heapq.heappush(queue,x)
+#     else:
+#         if not queue:
+#             print(0)
+#         else:
+#             print(heapq.heappop(queue))
+
+
+
+# https://www.acmicpc.net/problem/1406
+# import sys
+# s = sys.stdin.readline().strip()
+# m = int(input())
+# order_list = [sys.stdin.readline().split() for _ in range(m)]
+# current_idx = len(s)
+# for i in order_list:
+#     if i[0] == 'L':
+#         if current_idx == 0:
+#             continue
+#         else:
+#             current_idx -= 1
+#     elif i[0] == 'D':
+#         if current_idx == len(s):
+#             continue
+#         else:
+#             current_idx += 1
+#     elif i[0] == 'B':
+#         if current_idx == 0:
+#             continue
+#         else:
+#             s = s[0:current_idx-1] + s[current_idx:]
+#             current_idx -= 1
+#     else:
+#         s = s[0:current_idx] + i[1] + s[current_idx:]
+#         current_idx += 1
+# print(s)
+
+
+
+# import sys 
+# left_stack = list(sys.stdin.readline()[:-1]) 
+# right_stack = list() 
+# testcase = int(sys.stdin.readline()) 
+# for _ in range(testcase): 
+#     cmd = sys.stdin.readline() 
+#     if cmd[0] == 'L' and left_stack: 
+#         right_stack.append(left_stack.pop()) 
+#     elif cmd[0] == 'D' and right_stack: 
+#         left_stack.append(right_stack.pop()) 
+#     elif cmd[0] == 'B' and left_stack: 
+#         left_stack.pop() 
+#     elif cmd[0] == 'P': 
+#         left_stack.append(cmd[2]) 
+# sys.stdout.write(''.join(left_stack + right_stack[::-1]))
+
+
+
+
+
+
 
