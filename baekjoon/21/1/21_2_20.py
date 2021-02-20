b899df5a3d7c968045110dc61f7807bd2f1f145c
MinChul-Son <smc5236@naver.com>
2021-02-20 17:05:05 +0900

    21/2/20 algorithm_study4

--- baekjoon/21/1/21_2_20.py
+++ baekjoon/21/1/21_2_20.py
@@ -79,26 +79,109 @@
 
 
 # https://www.acmicpc.net/problem/5430
-import sys
-from collections import deque
-t = int(input())
+# import sys
+# from collections import deque
+# t = int(input())
 
-for _ in range(t):
-    p = sys.stdin.readline().strip()
-    n = int(sys.stdin.readline())
-    temp_list = sys.stdin.readline().strip().split(",")
-    if p.count('D') > n:
-        print("error")
-        continue
-    temp_list[0] = temp_list[0][1:]
-    temp_list[-1] = temp_list[-1][:-1]
-    temp_list = deque(temp_list)
-    for i in p:
-        if i == 'R':
-            temp_list.reverse()
-        else:
-            temp_list.popleft()
-    temp_list = list(map(int, temp_list))
-    print(temp_list)
+# for _ in range(t):
+#     p = sys.stdin.readline().strip()
+#     n = int(sys.stdin.readline())
+#     temp_list = sys.stdin.readline().strip().split(",")
+#     if p.count('D') > n:
+#         print("error")
+#         continue
+#     temp_list[0] = temp_list[0][1:]
+#     temp_list[-1] = temp_list[-1][:-1]
+#     temp_list = deque(temp_list)
+#     for i in p:
+#         if i == 'R':
+#             temp_list.reverse()
+#         else:
+#             temp_list.popleft()
+#     temp_list = list(map(int, temp_list))
+#     print(temp_list)
 
 
+
+# import sys 
+# import re 
+# from collections import deque 
+# T = int(sys.stdin.readline()) 
+# for _ in range(T): 
+#     P = list(sys.stdin.readline()) 
+#     N = int(sys.stdin.readline()) 
+#     input = sys.stdin.readline() 
+#     arr = re.split("\[|\]|,|\n", input) 
+#     print(arr)
+#     arr = deque(arr[1:-2]) 
+#     if N == 0: 
+#         arr = deque([]) 
+#     r_flag = False 
+#     oob = False 
+#     for p in P: 
+#         if p == "R": 
+#             r_flag = not r_flag 
+#         elif p == "D": 
+#             if len(arr) == 0: 
+#                 oob = True 
+#                 break 
+#             if r_flag: 
+#                 arr.pop() 
+#             else: 
+#                 arr.popleft() 
+#     if oob: 
+#         print("error") 
+#     else: 
+#         arr = list(arr) 
+#         if r_flag: 
+#             arr.reverse() 
+#         print("[", end = "") 
+#         print(",".join(arr), end = "") 
+#         print("]")
+
+
+
+
+# case = int(input())
+
+# for _ in range(case):
+#     function = list(input())
+#     num = int(input())
+#     arr = eval(input())
+    
+#     error = False
+#     R_count = 0 #홀/짝 뒤집힌 횟수용
+#     D_front = 0 #앞에서 없어지는 수
+    
+#     for func in function:            
+#         if func == 'R':
+#             R_count += 1
+#         else:
+#             try:
+#                 if R_count % 2 == 0:
+#                     D_front += 1 #앞에서는 슬라이싱으로 나중에 뺴줌
+#                 else:
+#                     arr.pop() #이건 뒤에서 바로 뺴줌
+#             except:
+#                 error = True
+#                 break
+                
+#     #에러 걸러주기
+#     if error or D_front > len(arr):
+#         print('error')
+#         continue
+        
+#     #R개수에 따른 정답 변형
+#     if R_count % 2 == 0:
+#         answer = arr[D_front:]
+#     else:
+#         answer = list(reversed(arr[D_front:]))
+        
+#     #출력함수
+#     print("[", end='')
+#     for i in range(len(answer)):
+#         if i == len(answer) - 1:
+#             print(answer[i], end = '')
+#         else:
+#             print("%s," %(answer[i]), end='')
+#     print("]")
