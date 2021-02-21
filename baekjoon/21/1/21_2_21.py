33469c05d0af02fb03cbfafe135702c7a8e5c5ab
MinChul-Son <smc5236@naver.com>
2021-02-21 15:25:20 +0900

    21/2/21 algorithm_study1

---
+++ baekjoon/21/1/21_2_21.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11286
+import sys
+import heapq
+n = int(input())
+num_list =[int(sys.stdin.readline()) for _ in range(n)]
+heap = []
+for i in num_list:
+    if i != 0:
+        heapq.heappush(heap, (abs(i), i))
+    else:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+
+
