867ff8c4ac2502d44a4a88cafddb610bca3a7806
MinChul-Son <smc5236@naver.com>
2021-02-22 20:21:41 +0900

    21/2/22 algorithm_study7

--- baekjoon/21/1/21_2_22.py
+++ baekjoon/21/1/21_2_22.py
@@ -68,15 +68,15 @@
 
 # https://www.acmicpc.net/problem/11722
 
-n = int(input())
-n_list = list(map(int, input().split()))
+# n = int(input())
+# n_list = list(map(int, input().split()))
 
-dp = [1 for _ in range(n)]
+# dp = [1 for _ in range(n)]
 
-for i in range(n):
-    for j in range(i):
-        if n_list[i] < n_list[j]:
-            dp[i] = max(dp[i], dp[j]+1)
-print(dp)
+# for i in range(n):
+#     for j in range(i):
+#         if n_list[i] < n_list[j]:
+#             dp[i] = max(dp[i], dp[j]+1)
+# print(max(dp))
 
 
