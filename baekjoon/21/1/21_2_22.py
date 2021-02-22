5556c83572f59acf36c934ff72811d31916a37ba
MinChul-Son <smc5236@naver.com>
2021-02-22 17:18:34 +0900

    21/2/22 algorithm_study2

--- baekjoon/21/1/21_2_22.py
+++ baekjoon/21/1/21_2_22.py
@@ -10,3 +10,19 @@
 #             dp[i] = dp[i-j*j]+1
 #         j += 1
 # print(dp[n])
+
+
+
+# https://www.acmicpc.net/problem/11726
+# n = int(input())
+
+# dp = [0 for _ in range(n+1)]
+
+# if n <= 3 : print(n)
+# else : 
+# 	dp[1] = 1
+# 	dp[2] = 2
+# 	for i in range(3, n+1):
+# 		dp[i] = dp[i-1] + dp[i-2]
+
+# 	print(dp[i]%10007)
