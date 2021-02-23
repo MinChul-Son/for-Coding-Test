583a171d0bdc290f48b8be66970eb151faa06acd
MinChul-Son <smc5236@naver.com>
2021-02-23 11:26:06 +0900

    21/2/23 pmcc2

--- PMCC2.py
+++ PMCC2.py
@@ -3,19 +3,17 @@ N = int(sys.stdin.readline())
 play_list = list(map(int, sys.stdin.readline().split()))
 Q = 0
 reverse_list = []
-if play_list == sorted(play_list):
-    print(0)
-else:
-    for i in range(N):
-        for j in range(N-1, i, -1):
-            if play_list[j-1] >play_list[j]:
-                play_list[j-1], play_list[j] = play_list[j], play_list[j-1]
-                Q += 1
-                reverse_list.append([play_list[j-1], play_list[j]])
+
+for i in range(N-1, 0, -1):
+    for j in range(0, i):
+        if play_list[j] >play_list[j+1]:
+            play_list[j], play_list[j+1] = play_list[j+1], play_list[j]
+            Q += 1
+            reverse_list.append([j+1, j+2])
         if Q > 100:
             print(-1)
             break
-    else:
-        print(Q)
-        for i in range(Q-1, -1, -1):
-            print(reverse_list[i][0], reverse_list[i][1])
+else:
+    print(Q)
+    for i,j in reverse_list:
+        print(i,j)
