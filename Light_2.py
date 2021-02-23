ae5126e342b15d5eec81983e121ae5768b97da4f
MinChul-Son <smc5236@naver.com>
2021-02-23 13:19:25 +0900

    pmcc6

---
+++ Light_2.py
@@ -0,0 +1,29 @@
+import sys
+N, K = map(int, input().split())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+pay = 0
+select = True
+if A[0] < B[0]:
+    pay += A[0]
+else:
+    pay += B[0]
+    select = False
+for i in range(1, N):
+    if select:
+        if A[i] > B[i] + K:
+            select = False
+            pay += B[i] + K
+            if i == N-1:
+                pay -= K
+        else:
+            pay += A[i]
+    else:
+        if A[i] + K < B[i]:
+            select = True
+            pay += A[i] + K
+            if i == N-1:
+                pay -= K
+        else:
+            pay += B[i]
+print(pay)
