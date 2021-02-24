bca6defe0cde0bd104e7abf1c926b248dc904583
MinChul-Son <smc5236@naver.com>
2021-02-24 16:05:33 +0900

    21/2/24 soma-test2

---
+++ soma_test.py
@@ -0,0 +1,19 @@
+# def main():
+#     b = input()
+#     if b.count('(') > b.count(')'):
+#         print('NO')
+#     else:
+#         print('YES')
+    
+
+# if __name__=="__main__":
+#     main()
+
+
+
+# insert into customer values(1, "Elice", "2010-01-15", 100);
+# insert into customer values(2, "Cheshire", "2005-03-10", 100);
+# insert into customer values(3, "Dodo", "2010-04-30", 100);
+# select * from customer;
+
+
