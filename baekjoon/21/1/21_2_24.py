b7ae3b149a519cd467ae6dbbba0b68c0a8f76dd5
MinChul-Son <smc5236@naver.com>
2021-02-24 17:49:20 +0900

    21/2/24 algorithm_study5

--- baekjoon/21/1/21_2_24.py
+++ baekjoon/21/1/21_2_24.py
@@ -24,27 +24,32 @@
 
 
 # https://www.acmicpc.net/problem/5525
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-N = int(input().rstrip())
-M = int(input().rstrip())
-S = input().rstrip()
+# N = int(input().rstrip())
+# M = int(input().rstrip())
+# S = input().rstrip()
 
-answer = 0
-count = 0
-i = 1
+# answer = 0
+# count = 0
+# i = 1
 
-while i < M - 1:
-    if S[i-1] == "I" and S[i] == "O" and S[i+1] == "I":
-        count += 1
-        if count == N:
-            count -= 1
-            answer += 1
-        i += 1
-    else:
-        count = 0
-    i += 1
+# while i < M - 1:
+#     if S[i-1] == "I" and S[i] == "O" and S[i+1] == "I":
+#         count += 1
+#         if count == N:
+#             count -= 1
+#             answer += 1
+#         i += 1
+#     else:
+#         count = 0
+#     i += 1
 
-print(answer)
+# print(answer)
+
+
+
+# https://www.acmicpc.net/problem/9461
+
 
