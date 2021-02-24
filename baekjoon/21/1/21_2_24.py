1c50b78d49878d0cb3bc6973135efa3f07ecb6a3
MinChul-Son <smc5236@naver.com>
2021-02-24 17:54:57 +0900

    21/2/24 algorithm_study6

--- baekjoon/21/1/21_2_24.py
+++ baekjoon/21/1/21_2_24.py
@@ -51,5 +51,14 @@
 
 
 # https://www.acmicpc.net/problem/9461
+wh = [0 for i in range(101)]
+wh[1] = 1
+wh[2] = 1
+wh[3] = 1
+for i in range(0, 98):
+    wh[i + 3] = wh[i] + wh[i + 1]
+t = int(input())
 
-
+for _ in range(t):
+    n = int(input())
+    print(wh[n])
