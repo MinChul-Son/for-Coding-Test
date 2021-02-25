8f51dca6c30ebdadedacf1ee183e0b656a1b10b5
MinChul-Son <smc5236@naver.com>
2021-02-25 15:49:52 +0900

    21/2/25 algorithm_study2

--- baekjoon/21/1/21_2_25.py
+++ baekjoon/21/1/21_2_25.py
@@ -1,14 +1,14 @@
 # https://www.acmicpc.net/problem/1759
-import sys
-from itertools import combinations
-l, c = map(int, sys.stdin.readline().split())
-alpha_list = sorted(list(sys.stdin.readline().split()))
-combi_list = list(combinations(alpha_list, l))
-vowels = ['a', 'e', 'i', 'o', 'u']
-for i in combi_list:
-    count = 0
-    for j in i:
-        if j in vowels:
-            count += 1
-    if (count >= 1) and (count <= l-2):
-        print(''.join(i))
+# import sys
+# from itertools import combinations
+# l, c = map(int, sys.stdin.readline().split())
+# alpha_list = sorted(list(sys.stdin.readline().split()))
+# combi_list = list(combinations(alpha_list, l))
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# for i in combi_list:
+#     count = 0
+#     for j in i:
+#         if j in vowels:
+#             count += 1
+#     if (count >= 1) and (count <= l-2):
+#         print(''.join(i))
