1da0eb72378bfbed06a329e174bdd04539db981c
MinChul-Son <smc5236@naver.com>
2021-02-25 16:45:43 +0900

    21/2/25 algorithm_study3

--- baekjoon/21/1/21_2_25.py
+++ baekjoon/21/1/21_2_25.py
@@ -12,3 +12,21 @@
 #             count += 1
 #     if (count >= 1) and (count <= l-2):
 #         print(''.join(i))
+
+
+# https://www.acmicpc.net/problem/1107
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+trouble_btn = set(sys.stdin.readline().split())
+btn = {str(i) for i in range(10)}
+btn = btn - trouble_btn
+result = abs(n-100)
+for i in range(1000001):
+    for part_num in str(i):
+        if (part_num not in btn):
+            break
+    else:
+        result = min(result, abs(n - i) + len(str(i)))
+
+print(result)
