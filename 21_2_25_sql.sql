853310e249715440b8f8a0463f38b8ee5b78148c
MinChul-Son <smc5236@naver.com>
2021-02-25 18:17:42 +0900

    21/2/25 sql_study3

--- 21_2_25_sql.sql
+++ 21_2_25_sql.sql
@@ -4,4 +4,10 @@ ON a.ANIMAL_ID = b.ANIMAL_ID
 WHERE a.ANIMAL_ID IS NULL;
 
 
+-- https://programmers.co.kr/learn/courses/30/lessons/59043?language=mysql
+SELECT a.ANIMAL_ID, a.NAME FROM ANIMAL_INS a INNER JOIN ANIMAL_OUTS b
+ON a.ANIMAL_ID = b.ANIMAL_ID
+WHERE a.DATETIME > b.DATETIME
+ORDER BY a.DATETIME;
+
 
