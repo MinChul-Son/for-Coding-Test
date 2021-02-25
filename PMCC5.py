b6f76a3a34022750779897b2c781fc4e720d08f1
MinChul-Son <smc5236@naver.com>
2021-02-25 14:06:55 +0900

    21/2/25 pmcc1_phase2

--- PMCC5.py
+++ PMCC5.py
@@ -1,15 +1,18 @@
 import sys
+from collections import deque
 N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().split()))
-while True:
+A = deque(map(int, sys.stdin.readline().split()))
+while len(A) > 2:
     if A[0] > A[-1]:
         A[0] -= 1
         A[2] -= 1
-        A.pop(1)
+        temp = A.popleft()
+        A.popleft()
+        A.appendleft(temp)
     else:
         A[-1] -= 1
         A[-3] -= 1
-        A.pop(-2)
-    if len(A) < 3:
-        break
+        temp = A.pop()
+        A.pop()
+        A.append(temp)
 print(max(A))
