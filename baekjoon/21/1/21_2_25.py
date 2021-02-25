718df13b5580ef29e4cf61fab83551c69974d6b6
MinChul-Son <smc5236@naver.com>
2021-02-25 16:51:24 +0900

    21/2/25 algorithm_study4

--- baekjoon/21/1/21_2_25.py
+++ baekjoon/21/1/21_2_25.py
@@ -15,18 +15,18 @@
 
 
 # https://www.acmicpc.net/problem/1107
-import sys
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
-trouble_btn = set(sys.stdin.readline().split())
-btn = {str(i) for i in range(10)}
-btn = btn - trouble_btn
-result = abs(n-100)
-for i in range(1000001):
-    for part_num in str(i):
-        if (part_num not in btn):
-            break
-    else:
-        result = min(result, abs(n - i) + len(str(i)))
+# import sys
+# n = int(sys.stdin.readline())
+# m = int(sys.stdin.readline())
+# trouble_btn = set(sys.stdin.readline().split())
+# btn = {str(i) for i in range(10)}
+# btn = btn - trouble_btn
+# result = abs(n-100)
+# for i in range(1000001):
+#     for part_num in str(i):
+#         if (part_num not in btn):
+#             break
+#     else:
+#         result = min(result, abs(n - i) + len(str(i)))
 
-print(result)
+# print(result)
