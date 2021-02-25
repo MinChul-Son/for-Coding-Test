d3c7cc67ca197390e9be64a4298ca188b9fda448
MinChul-Son <smc5236@naver.com>
2021-02-25 17:54:04 +0900

    21/2/25 sql_study2

--- 21_2_25_sql.sql
+++ 21_2_25_sql.sql
@@ -1,1 +1,7 @@
--- https://programmers.co.kr/learn/courses/30/parts/17046
+-- https://programmers.co.kr/learn/courses/30/lessons/59042?language=mysql
+SELECT b.ANIMAL_ID, b.NAME FROM ANIMAL_INS a RIGHT OUTER JOIN ANIMAL_OUTS b
+ON a.ANIMAL_ID = b.ANIMAL_ID
+WHERE a.ANIMAL_ID IS NULL;
+
+
+
