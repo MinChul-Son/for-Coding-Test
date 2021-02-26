001761eb345fe129332a96a8dae0cebeb66a08d8
MinChul-Son <smc5236@naver.com>
2021-02-26 17:37:42 +0900

    21/2/26 algorithm_study2

--- baekjoon/21/1/21_2_26.py
+++ baekjoon/21/1/21_2_26.py
@@ -1,11 +1,21 @@
 # https://www.acmicpc.net/problem/2156
-import sys
-n = int(sys.stdin.readline())
-wine_list = [int(sys.stdin.readline()) for _ in range(n)]
-dp = [0]
-dp.append(wine_list[0])
-if(n > 1):
-    dp.append(wine_list[0] + wine_list[1])
-for i in range(3, n+1):
-    dp.append(max(dp[i-1], dp[i-3]+wine_list[i-1]+wine_list[i-2],dp[i-2]+wine_list[i-1]))
-print(dp[-1])
+# import sys
+# n = int(sys.stdin.readline())
+# wine_list = [int(sys.stdin.readline()) for _ in range(n)]
+# dp = [0]
+# dp.append(wine_list[0])
+# if(n > 1):
+#     dp.append(wine_list[0] + wine_list[1])
+# for i in range(3, n+1):
+#     dp.append(max(dp[i-1], dp[i-3]+wine_list[i-1]+wine_list[i-2],dp[i-2]+wine_list[i-1]))
+# print(dp[-1])
+
+
+# https://www.acmicpc.net/problem/1904
+# n = int(input())
+# a, b = 0, 1
+# for i in range(n):
+#     a, b = b%15746, (a + b)%15746
+# print(b)
+
+
