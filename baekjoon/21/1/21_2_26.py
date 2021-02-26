e9cadbaca2e376d0ed0f9a3e10a5c57a5a15ac69
MinChul-Son <smc5236@naver.com>
2021-02-26 18:00:59 +0900

    21/2/26 algorithm_study3

--- baekjoon/21/1/21_2_26.py
+++ baekjoon/21/1/21_2_26.py
@@ -19,3 +19,16 @@
 # print(b)
 
 
+# https://www.acmicpc.net/problem/2293
+# import sys
+# n, k = map(int, sys.stdin.readline().split())
+# coin_list = [int(sys.stdin.readline()) for _ in range(n)]
+# dp = [0 for _ in range(k+1)] 
+# dp[0] = 1
+# for i in coin_list:
+#     for j in range(1, k + 1):
+#         if j - i >= 0:
+#             dp[j] += dp[j - i]
+# print(dp[k])
+
+
