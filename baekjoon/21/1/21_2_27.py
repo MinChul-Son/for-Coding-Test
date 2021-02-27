5219bcd4094581e47a669817c4c1acb66377464e
MinChul-Son <smc5236@naver.com>
2021-02-27 20:16:55 +0900

    21/2/27 algorithm_study3

--- baekjoon/21/1/21_2_27.py
+++ baekjoon/21/1/21_2_27.py
@@ -6,20 +6,20 @@ stack = [[] for _ in range (7)]
 move = 0
 for i,j in melody_list:
     if not stack[i]:
-        stack.append(j)
-        move += 1
-        continue
-    if j > stack[i][-1]:
-        stack.append(j)
-        move += 1
-    elif j == stack[i][-1]:
-        continue
-    else:
-        while len(stack[i]) != 0 and j < stack[i][-1]:
-            stack[i].pop()
-            move += 1
-        if len(stack[i]) != 0 and j == stack[i][-1]:
-            continue
         stack[i].append(j)
         move += 1
+    else:
+        if j > stack[i][-1]:
+            stack[i].append(j)
+            move += 1
+        elif j == stack[i][-1]:
+            continue
+        else:
+            while stack[i] and j < stack[i][-1]:
+                stack[i].pop()
+                move += 1
+            if stack[i] and j == stack[i][-1]:
+                continue
+            stack[i].append(j)
+            move += 1
 print(move)
