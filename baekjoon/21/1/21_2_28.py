2f63699845ead5ef5bfdc7f088448744132fb89b
MinChul-Son <smc5236@naver.com>
2021-02-28 15:54:01 +0900

    21/2/28 algorithm_study2

--- baekjoon/21/1/21_2_28.py
+++ baekjoon/21/1/21_2_28.py
@@ -1,1 +1,31 @@
 # https://www.acmicpc.net/problem/1260
+from collections import deque
+def bfs(edge_list,visited, start):
+    queue = deque([start])
+    visited[start] = False
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in range(1, n+1):
+            if visited[i] and edge_list[v][i]:
+                queue.append(i)
+                visited[i] = False
+
+def dfs(edge_list,visited, start):
+    visited[start] = True
+    print(start, end=" ")
+    for i in range(1, n+1):
+        if not visited[i] and edge_list[start][i]:
+            dfs(edge_list, visited, i)
+
+n, m, v = map(int, input().split())
+
+edge_list = [[0]* (n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    edge_list[a][b] = 1
+    edge_list[b][a] = 1
+visited = [False for _ in range(n+1)]
+dfs(edge_list, visited, v)
+print()
+bfs(edge_list, visited, v)
