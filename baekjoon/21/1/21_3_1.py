6dc8eee79950f53bf652ae6201b939aad2597ed9
MinChul-Son <smc5236@naver.com>
2021-03-01 21:26:52 +0900

    21/3/1 algorithm_study6

--- baekjoon/21/1/21_3_1.py
+++ baekjoon/21/1/21_3_1.py
@@ -34,23 +34,27 @@
 
 
 # https://www.acmicpc.net/problem/11724
-import sys
-sys.setrecursionlimit(10000)
-def dfs(i):
-    visited[i] = True
-    for j in range(1, n + 1):
-        if graph[i][j] and not visited[j]:
-            dfs(j)
-n, m = map(int, sys.stdin.readline().split())
-graph = [[0] * (n+1) for _ in range(n+1)]
-for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
-visited = [False for _ in range(n+1)]
-result = 0
-for i in range(1, n+1):
-    if not visited[i]: 
-        dfs(i)
-        result += 1
-print(result)
+# import sys
+# sys.setrecursionlimit(10000)
+# def dfs(i):
+#     visited[i] = True
+#     for j in range(1, n + 1):
+#         if graph[i][j] and not visited[j]:
+#             dfs(j)
+# n, m = map(int, sys.stdin.readline().split())
+# graph = [[0] * (n+1) for _ in range(n+1)]
+# for _ in range(m):
+#     a, b = map(int, sys.stdin.readline().split())
+#     graph[a][b] = 1
+#     graph[b][a] = 1
+# visited = [False for _ in range(n+1)]
+# result = 0
+# for i in range(1, n+1):
+#     if not visited[i]: 
+#         dfs(i)
+#         result += 1
+# print(result)
+
+
+
+# https://www.acmicpc.net/problem/6603
