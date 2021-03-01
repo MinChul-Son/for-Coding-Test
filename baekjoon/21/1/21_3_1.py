ee22e9a6502d6f9b5517c1cf911e01d85990ab4f
MinChul-Son <smc5236@naver.com>
2021-03-01 20:47:21 +0900

    21/3/1 algorithm_study3

--- baekjoon/21/1/21_3_1.py
+++ baekjoon/21/1/21_3_1.py
@@ -1,12 +1,14 @@
 # https://www.acmicpc.net/problem/1012
+import sys
+sys.setrecursionlimit(10**6)
 def dfs(x, y):
     #주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] == 0:
+    if graph[x][y] == 1:
         # 해당 노드 방문 처리
-        graph[x][y] = 1
+        graph[x][y] = 2
         # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
         dfs(x -1, y)
         dfs(x, y -1)
@@ -22,9 +24,9 @@ for _ in range(t):
         a, b = map(int, input().split())
         graph[b][a] = 1
     result = 0
-    for i in range(m):
-        for j in range(n):
+    for i in range(n):
+        for j in range(m):
             #현재 위치에서 DFS 수행
             if dfs(i,j) == True:
                 result += 1
-                print(result)
+    print(result)
