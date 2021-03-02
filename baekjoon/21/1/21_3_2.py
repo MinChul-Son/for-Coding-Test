bbfcb859e27913801f4e7bb4b7c455db923670e1
MinChul-Son <smc5236@naver.com>
2021-03-02 17:59:44 +0900

    21/3/2 algorithm_study9

--- baekjoon/21/1/21_3_2.py
+++ baekjoon/21/1/21_3_2.py
@@ -50,4 +50,26 @@
 #     print("SK")
 
 
+# https://www.acmicpc.net/problem/11725
+import sys 
+sys.setrecursionlimit(100000) #런타임 에러 방지 
+n = int(input()) 
+tree = [[] for _ in range(n+1)] # 양방향으로 노드를 연결하고 
+for _ in range(n-1): # 노드의 수 - 1 (간선) 
+    a, b = map(int, input().split()) 
+    tree[a].append(b) 
+    tree[b].append(a) # 각 노드의 부모 노드 값 저장용 
+parents = [0 for _ in range(n+1)] 
+parents[1] = 1 
+def dfs(curr, tree, parents):  
+    for node in tree[curr]: 
+        if parents[node] == 0: 
+            parents[node] = curr # 해당 자식 노드에 부모노드(curr) 값 넣음 
+            dfs(node, tree, parents) # 자식 노드가 서브 트리 루트로서 탐색 
+dfs(1, tree, parents) 
+for i in range(2, n+1):
+    print(parents[i])
+
+
+
 
