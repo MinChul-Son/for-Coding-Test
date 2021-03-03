10a66d3b701f775594d3ac7f395d35141b12368e
MinChul-Son <smc5236@naver.com>
2021-03-03 18:09:04 +0900

    21/3/3 algorithm_study2

--- baekjoon/21/1/21_3_3.py
+++ baekjoon/21/1/21_3_3.py
@@ -1,1 +1,12 @@
-# https://www.acmicpc.net/problem/3085
+# https://www.acmicpc.net/problem/1051
+n, m = map(int, input().split())
+num_list = [list(map(int, input())) for _ in range(n)]
+answer = 0
+for i in range(n):
+    for j in range(m):
+        for k in range(n if n < m else m):
+            if i+k < n and j+k < m:
+                if num_list[i][j] == num_list[i][j+k] == num_list[i+k][j] == num_list[i+k][j+k]:
+                    if answer < k:
+                        answer = k
+print((answer+1)**2)
