e182cfd0bbf5ea892d1c750c0313fc3b92fcee4d
MinChul-Son <smc5236@naver.com>
2021-03-03 18:25:23 +0900

    21/3/3 algorithm_study3

--- baekjoon/21/1/21_3_3.py
+++ baekjoon/21/1/21_3_3.py
@@ -1,12 +1,27 @@
 # https://www.acmicpc.net/problem/1051
-n, m = map(int, input().split())
-num_list = [list(map(int, input())) for _ in range(n)]
-answer = 0
-for i in range(n):
-    for j in range(m):
-        for k in range(n if n < m else m):
-            if i+k < n and j+k < m:
-                if num_list[i][j] == num_list[i][j+k] == num_list[i+k][j] == num_list[i+k][j+k]:
-                    if answer < k:
-                        answer = k
-print((answer+1)**2)
+# n, m = map(int, input().split())
+# num_list = [list(map(int, input())) for _ in range(n)]
+# answer = 0
+# for i in range(n):
+#     for j in range(m):
+#         for k in range(n if n < m else m):
+#             if i+k < n and j+k < m:
+#                 if num_list[i][j] == num_list[i][j+k] == num_list[i+k][j] == num_list[i+k][j+k]:
+#                     if answer < k:
+#                         answer = k
+# print((answer+1)**2)
+
+
+
+# https://www.acmicpc.net/problem/1543
+import sys
+document = sys.stdin.readline().strip()
+search_word = sys.stdin.readline().strip()
+count = 0
+while True:
+    if search_word in document:
+        count += 1
+        document = document.replace(search_word, "", 1)
+    else:
+        break
+print(count)
