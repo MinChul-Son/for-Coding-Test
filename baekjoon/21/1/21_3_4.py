a84e02f8c07520818187b9ae34c1d9d70f122294
MinChul-Son <smc5236@naver.com>
2021-03-04 15:11:32 +0900

    21/3/4 algorithm_study2

---
+++ baekjoon/21/1/21_3_4.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11441
+import sys
+n = int(sys.stdin.readline())
+num_list = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+m_list = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+cum = [num_list[0]]
+for i in range(1, n):
+    cum.append(cum[-1]+num_list[i])
+for i, j in m_list:
+    if i == 1:
+        print(cum[j-1])
+    else:
+        print(cum[j-1]-cum[i-2])
