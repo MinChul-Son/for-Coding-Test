4a7e65999a7cd841a90e736051c75fe8c6f3c71a
MinChul-Son <smc5236@naver.com>
2021-03-04 16:33:31 +0900

    21/3/4 algorithm_study10

--- baekjoon/21/1/21_3_4.py
+++ baekjoon/21/1/21_3_4.py
@@ -61,3 +61,16 @@
 #     combi_list = list(combinations(num_list, m))
 #     for i in sorted(list(set(combi_list))):
 #         print(*i)
+
+
+# https://www.acmicpc.net/problem/15666
+# from itertools import permutations
+# n, m = map(int, input().split())
+# num_list = sorted(list(map(int, input().split())))
+# if m == 1:
+#     for i in sorted(list(set(num_list))):
+#         print(i)
+# else:
+#     permu_list = list(permutations(num_list, m))
+#     for i in sorted(list(set(permu_list))):
+#         print(*i)
