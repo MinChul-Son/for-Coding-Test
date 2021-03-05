f8dd4dcc5aff546ded64c0878f4acc3e45b60ba3
MinChul-Son <smc5236@naver.com>
2021-03-05 16:37:06 +0900

    21/3/5 algorithm_study7

--- baekjoon/21/1/21_3_5.py
+++ baekjoon/21/1/21_3_5.py
@@ -50,11 +50,9 @@
 
 # https://www.acmicpc.net/problem/10826
 n = int(input())
-dp = [0 for _ in range(n+1)]
-dp[0] = 0
-dp[1] = 1
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+dp = [0, 1, 1]
+for i in range(3, n+1):
+    dp.append(dp[i-2]+dp[i-1])
 print(dp[n])
 
 
