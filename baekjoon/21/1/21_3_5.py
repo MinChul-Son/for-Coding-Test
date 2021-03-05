9a70b17b86398d95cf567b10799446239cd28067
MinChul-Son <smc5236@naver.com>
2021-03-05 14:58:50 +0900

    21/3/5 algorithm_study3

--- baekjoon/21/1/21_3_5.py
+++ baekjoon/21/1/21_3_5.py
@@ -1,10 +1,28 @@
 # https://www.acmicpc.net/problem/2644
 import sys
+from collections import deque
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visit = [0 for i in range(n + 1)]
+    visit[start] = 1
+    while queue:
+        d = queue.popleft()
+        for i in tree[d]:
+            if visit[i] == 0:
+                visit[i] = 1
+                result[i] = result[d] + 1
+                queue.append(i)
 n = int(sys.stdin.readline())
-cal_num = list(map(int, sys.stdin.readline().split()))
+cal_num1, cal_num2 = map(int, sys.stdin.readline().split())
 m = int(sys.stdin.readline())
 tree = [[] for _ in range(n+1)]
+result = [0 for i in range(n + 1)]
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     tree[a].append(b)
+    tree[b].append(a)
 print(tree)
+bfs(cal_num1)
+print(result[cal_num2] if result[cal_num2] != 0 else -1)
+
