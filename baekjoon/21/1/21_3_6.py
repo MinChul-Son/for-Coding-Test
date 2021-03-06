6f29bdfcfb9d9c7a32c726912d0efd703c43ee9b
MinChul-Son <smc5236@naver.com>
2021-03-06 17:16:56 +0900

    21/3/6 algorithm_study6

--- baekjoon/21/1/21_3_6.py
+++ baekjoon/21/1/21_3_6.py
@@ -16,16 +16,18 @@
 
 
 # https://www.acmicpc.net/problem/1120
-a, b = input().split()
-b = list(b)
-len_a = len(a)
-max_cnt = 0
-while len_a <= len(b):
-    cnt = 0
-    for i in range(len_a):
-        if a[i] == b[i]:
-            cnt += 1
-    max_cnt = max(max_cnt, cnt)
-    print(a,b)
-    del b[0]
-print(len_a - max_cnt)
+# a, b = input().split()
+# b = list(b)
+# len_a = len(a)
+# max_cnt = 0
+# while len_a <= len(b):
+#     cnt = 0
+#     for i in range(len_a):
+#         if a[i] == b[i]:
+#             cnt += 1
+#     max_cnt = max(max_cnt, cnt)
+#     print(a,b)
+#     del b[0]
+# print(len_a - max_cnt)
+
+# https://www.acmicpc.net/problem/17413
