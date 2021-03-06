ae1641df5af32d0150a86233004645532c3b13de
MinChul-Son <smc5236@naver.com>
2021-03-06 16:56:21 +0900

    21/3/6 algorithm_study4

--- baekjoon/21/1/21_3_6.py
+++ baekjoon/21/1/21_3_6.py
@@ -1,16 +1,14 @@
 # https://www.acmicpc.net/problem/10844
-stair_numbers = [[0 for _ in range(10)] for _ in range(101)]
-for i in range(1, 101):
+n = int(input())
+dp = [[0 for i in range(10)] for j in range(101)]
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n + 1):
     for j in range(10):
-        if i == 1:
-            stair_numbers[i][j] = 1
+        if j == 0:
+            dp[i][j] = dp[i - 1][1]
+        elif j == 9:
+            dp[i][j] = dp[i - 1][8]
         else:
-            if 1 <= j <= 8:
-                stair_numbers[i][j] = stair_numbers[i-1][j-1] + stair_numbers[i-1][j+1]
-            elif j == 0:
-                stair_numbers[i][j] = stair_numbers[i-1][j+1]
-            elif j == 9:
-                stair_numbers[i][j] = stair_numbers[i-1][j-11]
-                
-N = int(input())             
-print(sum(stair_numbers[N][1:10]) % 1000000000)
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+print(sum(dp[n]) % 1000000000)
