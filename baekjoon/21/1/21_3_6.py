8d941b15bcd6c1d99e02bffc405506d8e3c6c73a
MinChul-Son <smc5236@naver.com>
2021-03-06 19:45:45 +0900

    21/3/6 algorithm_study12

--- baekjoon/21/1/21_3_6.py
+++ baekjoon/21/1/21_3_6.py
@@ -59,20 +59,20 @@
 
 
 # https://www.acmicpc.net/problem/1305
-L = int(input())
-current_s = input()
-s_len = len(current_s)
+# L = int(input())
+# current_s = input()
+# s_len = len(current_s)
 
-p_table = [0 for _ in range(s_len)]
-j = 0
-for i in range(1, s_len):
-    while j > 0 and current_s[i] != current_s[j]:
-        j = p_table[j-1]
-    if current_s[i] ==current_s[j]:
-        j += 1
-        p_table[i] = j
-p_len = s_len - p_table[s_len - 1]
-print(p_len)
+# p_table = [0 for _ in range(s_len)]
+# j = 0
+# for i in range(1, s_len):
+#     while j > 0 and current_s[i] != current_s[j]:
+#         j = p_table[j-1]
+#     if current_s[i] ==current_s[j]:
+#         j += 1
+#         p_table[i] = j
+# p_len = s_len - p_table[s_len - 1]
+# print(p_len)
 
 
 
