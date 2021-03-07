2fa1725c401eca554147f5120ede512f754c0e6c
MinChul-Son <smc5236@naver.com>
2021-03-07 17:33:56 +0900

    21/3/7 algorithm_study3

--- baekjoon/21/1/21_3_7.py
+++ baekjoon/21/1/21_3_7.py
@@ -1,19 +1,40 @@
 # https://www.acmicpc.net/problem/5052
+# import sys
+# input = sys.stdin.readline
+# a = [0] + list(input().strip())
+# b = [0] + list(input().strip())
+# len_a = len(a)
+# len_b = len(b)
+# dp = [[""] * len_b for i in range(len_a)]
+# for i in range(1, len_a):
+#     for j in range(1, len_b):
+#         if a[i] == b[j]:
+#             dp[i][j] = dp[i - 1][j - 1] + a[i]
+#         else:
+#             if len(dp[i - 1][j]) > len(dp[i][j - 1]):
+#                 dp[i][j] = dp[i - 1][j]
+#             else:
+#                 dp[i][j] = dp[i][j - 1]
+# print(len(dp[len_a - 1][len_b - 1]))
+# print(dp[len_a - 1][len_b - 1])
+
+
+# https://www.acmicpc.net/problem/4358
 import sys
-input = sys.stdin.readline
-a = [0] + list(input().strip())
-b = [0] + list(input().strip())
-len_a = len(a)
-len_b = len(b)
-dp = [[""] * len_b for i in range(len_a)]
-for i in range(1, len_a):
-    for j in range(1, len_b):
-        if a[i] == b[j]:
-            dp[i][j] = dp[i - 1][j - 1] + a[i]
-        else:
-            if len(dp[i - 1][j]) > len(dp[i][j - 1]):
-                dp[i][j] = dp[i - 1][j]
-            else:
-                dp[i][j] = dp[i][j - 1]
-print(len(dp[len_a - 1][len_b - 1]))
-print(dp[len_a - 1][len_b - 1])
+tree_dic = dict()
+len_list = 0
+
+while True:
+    tree = sys.stdin.readline().strip()
+    if not tree:
+        break
+    len_list += 1
+    if tree in tree_dic:
+        tree_dic[tree] += 1
+    else:
+        tree_dic[tree] = 1
+sorted_tree = sorted(tree_dic.items())
+for i in sorted_tree:
+    print(i[0], end=" ")
+    print("%.4f" %((i[1]/len_list)*100))
+
