f9483a3610ec585ff6b472f6ee6f610e7b2ca01e
MinChul-Son <smc5236@naver.com>
2021-03-10 13:40:13 +0900

    21/3/10 algorithm_study3

--- baekjoon/21/1/21_3_10.py
+++ baekjoon/21/1/21_3_10.py
@@ -1,17 +1,22 @@
 # https://www.acmicpc.net/problem/9184
-import sys
-sys.setrecursionlimit(10**6)
 def solution(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
     elif a > 20 or b > 20 or c > 20:
         return solution(20, 20, 20)
-    elif a < b and b < c:
-        return (solution(a, b, c-1) + solution(a, b-1, c-1) - solution(a, b-1, c))
-    else:
-        return (solution(a-1, b, c) + solution(a-1, b-1, c) + solution(a-1, b, c-1) - solution(a-1, b-1, c-1))
+
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a<b<c :
+        dp[a][b][c] = solution(a,b,c-1) + solution(a,b-1,c-1) - solution(a,b-1,c)
+        return dp[a][b][c]
+    dp[a][b][c] = solution(a-1,b,c) + solution(a-1,b-1,c) + solution(a-1,b,c-1) - solution(a-1,b-1,c-1)
+    return dp[a][b][c]
+    
+
 while True:
     a, b, c = map(int, input().split())
+    dp = [[[0]*21 for _ in range(21)] for __ in range(21)]
     if a == -1 and b == -1 and c == -1:
         break
-    print("w("+ str(a) + ", "+ str(b) + ", " + str(c) + ") = " + " " + str(solution(a,b,c)))
+    print("w(%d, %d, %d) = %d"%(a, b, c, solution(a, b, c)))
