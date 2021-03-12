535b359e924caeebb1afdd28741e1d4f60e3e00f
MinChul-Son <smc5236@naver.com>
2021-03-12 16:03:38 +0900

    알고리즘 문제풀이: 백준 > 4963번 섬의 개수
    이번에는 DFS를 재귀함수로 구현하였다.
    이번에는 대각선으로 이동하는 경우도 탐색해야하므로 총 8방향을
    탐색하였다.
    파이썬은 재귀 깊이가 얕기 때문에 깊이제한을 늘려줘야 문제해결가능하다.
    이왕이면 그냥 파이썬을 사용하여 그래프탐색을 할때는 deque도 있으니 BFS를
    사용하는 것이 더 좋을 것 같다.

--- baekjoon/21/1/21_3_12.py
+++ baekjoon/21/1/21_3_12.py
@@ -1,35 +1,69 @@
-def preorder(node):
-    if node == '.':
-        return
-    print(node, end="")
-    preorder(tree[node][0]) #왼쪽 서브트리
-    preorder(tree[node][1]) #오른쪽 서브트리
+# https://www.acmicpc.net/problem/1991
 
-def inorder(node):
-    if node == '.':
-        return
-    inorder(tree[node][0])
-    print(node, end="")
-    inorder(tree[node][1])
+# def preorder(node):
+#     if node == '.':
+#         return
+#     print(node, end="")
+#     preorder(tree[node][0]) #왼쪽 서브트리
+#     preorder(tree[node][1]) #오른쪽 서브트리
 
-def postorder(node):
-    if node == '.':
-        return
-    postorder(tree[node][0])
-    postorder(tree[node][1])
-    print(node, end="")
+# def inorder(node):
+#     if node == '.':
+#         return
+#     inorder(tree[node][0])
+#     print(node, end="")
+#     inorder(tree[node][1])
 
-n = int(input()) 
-tree = {}
-for _ in range(n): 
-    root, left, right = input().split() 
-    tree[root] = (left, right)
-print(tree)
-preorder('A') 
-print() 
-inorder('A') 
-print() 
-postorder('A')
+# def postorder(node):
+#     if node == '.':
+#         return
+#     postorder(tree[node][0])
+#     postorder(tree[node][1])
+#     print(node, end="")
+
+# n = int(input()) 
+# tree = {}
+# for _ in range(n): 
+#     root, left, right = input().split() 
+#     tree[root] = (left, right)
+# preorder('A') 
+# print() 
+# inorder('A') 
+# print() 
+# postorder('A')
+
+
+# https://www.acmicpc.net/problem/4963
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(x, y):
+    if x <= -1 or x >= h or y <= -1 or y >= w:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 2
+        for i in range(8):
+            a = x + move_x[i]
+            b = y + move_y[i]      
+            dfs(a, b)   
+        return True
+    return False
+
+
+while True:
+    w, h = map(int, input().split())
+    if w == h == 0:
+        break
+
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    move_x = [1, -1, 0, 0, 1, -1, -1, 1]
+    move_y = [0, 0, -1, 1, 1, 1, -1, -1]
+    result = 0
+    for i in range(h):
+        for j in range(w):
+            if dfs(i,j) == True:
+                result += 1
+    print(result)
 
 
 
