41a89a4646b654f1c15f41b060fd08a7cec0fcdd
MinChul-Son <smc5236@naver.com>
2021-03-12 16:36:13 +0900

    알고리즘 문제풀이 : 백준 > 2003번 수들의 합2
    해당 문제를 2중 for문을 사용하여 i값보다 오른쪽에 있는 수들과 더하여
    m값과 비교하는 식으로 구현했다. 백준 기준으로 Python3로 제출하니
    시간초과 pypy로 제출하니 겨우겨우 통과할 수 있었다.
    찜찜해서 찾아보니 이 문제는 두 포인터를 사용하여 해결하는 문제라한다.
    그래서 두 포인터를 사용하여 다시 풀어봐야겠다..ㅎㅎ

--- baekjoon/21/1/21_3_12.py
+++ baekjoon/21/1/21_3_12.py
@@ -34,36 +34,53 @@
 
 
 # https://www.acmicpc.net/problem/4963
+# import sys
+# sys.setrecursionlimit(10**6)
+
+# def dfs(x, y):
+#     if x <= -1 or x >= h or y <= -1 or y >= w:
+#         return False
+#     if graph[x][y] == 1:
+#         graph[x][y] = 2
+#         for i in range(8):
+#             a = x + move_x[i]
+#             b = y + move_y[i]      
+#             dfs(a, b)   
+#         return True
+#     return False
+
+
+# while True:
+#     w, h = map(int, input().split())
+#     if w == h == 0:
+#         break
+
+#     graph = [list(map(int, input().split())) for _ in range(h)]
+#     move_x = [1, -1, 0, 0, 1, -1, -1, 1]
+#     move_y = [0, 0, -1, 1, 1, 1, -1, -1]
+#     result = 0
+#     for i in range(h):
+#         for j in range(w):
+#             if dfs(i,j) == True:
+#                 result += 1
+#     print(result)
+
+
+# https://www.acmicpc.net/problem/2003
 import sys
-sys.setrecursionlimit(10**6)
-
-def dfs(x, y):
-    if x <= -1 or x >= h or y <= -1 or y >= w:
-        return False
-    if graph[x][y] == 1:
-        graph[x][y] = 2
-        for i in range(8):
-            a = x + move_x[i]
-            b = y + move_y[i]      
-            dfs(a, b)   
-        return True
-    return False
-
-
-while True:
-    w, h = map(int, input().split())
-    if w == h == 0:
-        break
-
-    graph = [list(map(int, input().split())) for _ in range(h)]
-    move_x = [1, -1, 0, 0, 1, -1, -1, 1]
-    move_y = [0, 0, -1, 1, 1, 1, -1, -1]
-    result = 0
-    for i in range(h):
-        for j in range(w):
-            if dfs(i,j) == True:
-                result += 1
-    print(result)
+n, m = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+answer = 0
+for i in range(n):
+    temp_sum = 0
+    for j in range(i, n):
+        temp_sum += nums[j]
+        if temp_sum == m:
+            answer += 1
+            break
+        if temp_sum > m:
+            break
+print(answer)
 
 
 
