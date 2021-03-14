57c971feb53b987135cc09e01a2a59e60d163835
MinChul-Son <smc5236@naver.com>
2021-03-14 18:22:37 +0900

    알고리즘 문제풀이 : 백준 > 14490번 백대열
    파이썬 math라이브러리의 gcd외장함수를 사용하여 문제를 해결.
    두 수의 gcd가 1이면 더이상 약분 불가능하다는 논리를 사용함.

--- baekjoon/21/1/21_3_14.py
+++ baekjoon/21/1/21_3_14.py
@@ -1,46 +1,59 @@
 # https://www.acmicpc.net/problem/7576
-from collections import deque
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-already_ripen = False
-isall_ripen = False
-for i in graph:
-    if 0 in i:
-        break
-else:
-    already_ripen = True
+# from collections import deque
+# m, n = map(int, input().split())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# already_ripen = False
+# isall_ripen = False
+# for i in graph:
+#     if 0 in i:
+#         break
+# else:
+#     already_ripen = True
 
-if already_ripen:
-    print(0)
-else:
-    queue = deque()
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                queue.append([i, j])
-    move_x = [1, -1, 0, 0]
-    move_y = [0, 0, -1, 1]
-    while queue:
-        a, b = queue.popleft()
-        for i in range(4):
-            x = a + move_x[i]
-            y = b + move_y[i]
-            if 0 <= x < n and 0 <= y < m and graph[x][y] == 0:
-                queue.append([x, y])
-                graph[x][y] = graph[a][b] + 1
-    for i in graph:
-        if 0 in i:
-            break
-    else:
-        isall_ripen = True
-    if not isall_ripen:
-        print(-1)
-    else:
-        result = 0
-        for i in graph:
-            for j in i:
-                if j > result:
-                    result = j
-        print(result-1)
+# if already_ripen:
+#     print(0)
+# else:
+#     queue = deque()
+#     for i in range(n):
+#         for j in range(m):
+#             if graph[i][j] == 1:
+#                 queue.append([i, j])
+#     move_x = [1, -1, 0, 0]
+#     move_y = [0, 0, -1, 1]
+#     while queue:
+#         a, b = queue.popleft()
+#         for i in range(4):
+#             x = a + move_x[i]
+#             y = b + move_y[i]
+#             if 0 <= x < n and 0 <= y < m and graph[x][y] == 0:
+#                 queue.append([x, y])
+#                 graph[x][y] = graph[a][b] + 1
+#     for i in graph:
+#         if 0 in i:
+#             break
+#     else:
+#         isall_ripen = True
+#     if not isall_ripen:
+#         print(-1)
+#     else:
+#         result = 0
+#         for i in graph:
+#             for j in i:
+#                 if j > result:
+#                     result = j
+#         print(result-1)
+
+
+
+# https://www.acmicpc.net/problem/14490
+from math import gcd
+n, m = map(int, input().split(':'))
+while True:
+    gcd_num = gcd(n, m)
+    if gcd_num == 1:
+        break
+    n //= gcd_num
+    m //= gcd_num
+print(str(n) + ":" + str(m))
 
 
