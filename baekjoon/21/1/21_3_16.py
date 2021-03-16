9cc12a1be0135d202f898fe93437fc05f5019aed
MinChul-Son <smc5236@naver.com>
2021-03-16 17:35:01 +0900

    알고리즘 문제풀이 : 백준 > 11048번 이동하기
    점화식을 도출해내어 dp(동적계획법)으로 문제를 해결하였다.

---
+++ baekjoon/21/1/21_3_16.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11048
+n, m = map(int, input().split())
+maze = [list(map(int, input().split())) for _ in range(n)]
+# max(maze[r-1][c], maze[r][c-1], maze[r-1][c-1])
+dp = [[0] * (m+1) for _ in range(n)]
+for i in range(1, m+1):
+    dp[0][i] += (maze[0][i-1] + dp[0][i-1])
+for i in range(1, n):
+    dp[i][1] += (maze[i][0] + dp[i-1][1])
+
+for i in range(1, n):
+    for j in range(2, m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + maze[i][j-1]
+print(dp[-1][-1])
