091465b1e96c40b037c16198520964ea4abe5a04
MinChul-Son <smc5236@naver.com>
2021-03-18 17:46:50 +0900

    알고리즘 공부 : 동빈북-DP > 1로 만들기 문제(나의풀이)
    바텀업 방식으로 DP

--- dp_study_dongbinbook.py
+++ dp_study_dongbinbook.py
@@ -38,3 +38,19 @@ dp[2] = max(k[0], k[1])
 for i in range(3, n + 1):
     dp[i] = max(dp[i-1], k[i-1]+ dp[i-2])
 print(dp[-1])
+
+
+
+# 1로 만들기 문제
+x = int(input())
+dp = [0 for _ in range(x+1)]
+for i in range(2, x+1):
+    dp[i] = dp[i-1] + 1
+
+    if i % 2 == 0 and dp[i] > dp[i//2] +1:
+        dp[i] = dp[i//2] + 1
+    if i % 3 == 0 and dp[i] > dp[i//3] +1:
+        dp[i] = dp[i//3] + 1
+    if i % 5 == 0 and dp[i] > dp[i//5] +1:
+        dp[i] = dp[i//5] + 1
+print(dp[x])
