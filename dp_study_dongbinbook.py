f4a9794ede5379ced4889c6cd9d2f3b7aea8c9bf
MinChul-Son <smc5236@naver.com>
2021-03-18 19:36:35 +0900

    알고리즘 공부 : 동빈북-DP > 금광 문제
    왼쪽에서 오는 경우, 왼쪽 위에서 오는경우, 왼쪽 아래에서 오는경우
    총 3가지의 경우를 점화식으로하여 max값을 구해준다.
    답안예시와 나의 풀이가 코드는 조금 다르지만 기본점화식은 동일

--- dp_study_dongbinbook.py
+++ dp_study_dongbinbook.py
@@ -85,3 +85,57 @@ if dp[m] == 10001:
     print(-1)
 else:
     print(dp[m])
+
+
+# 금광 문제
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    golds = list(map(int, input().split()))
+    graph = []
+    temp = 0
+    for i in range(n):
+        graph.append(golds[temp: temp + m])
+        temp += m
+    for i in range(1, m):
+        for j in range(n):
+            if j-1 < 0:
+                graph[j][i] += max(graph[j][i-1], graph[j+1][i-1])
+            elif j+1 == n:
+                graph[j][i] += max(graph[j-1][i-1], graph[j][i-1])
+            else:
+                graph[j][i] += max(graph[j-1][i-1], graph[j][i-1], graph[j+1][i-1])
+    answer = 0
+    for i in range(n):
+        answer = max(answer, graph[i][m-1])
+    print(answer)
+
+# 답안 예시
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+    dp = []
+    index = 0
+    for i in range(n):
+        dp.append(array[index: index + m])
+        index += m
+    for j in range(1, m):
+        for i in range(n):
+            #왼쪽 위에서 오는 경우
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            #왼쪽 아래에서 오는 경우
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+            #왼쪽에서 오는 경우
+            left = dp[i][j-1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+    answer = 0
+    for i in range(n):
+        answer = max(answer, dp[i][m-1])
+    print(answer)
