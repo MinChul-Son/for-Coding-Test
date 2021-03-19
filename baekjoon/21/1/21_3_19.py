a6e57a9b9ad7052b36fed3cb92c599cf19cb1ef8
MinChul-Son <smc5236@naver.com>
2021-03-19 17:29:39 +0900

    알고리즘 문제풀이 : 백준 > 10451번 순열 사이클
    deque를 통하여 큐를 구현하고, bfs를 수행하였다.
    처음 시작노드를 기준으로 다시 돌아온다며 한 cycle로 판단했다.

--- baekjoon/21/1/21_3_19.py
+++ baekjoon/21/1/21_3_19.py
@@ -1,32 +1,59 @@
 # https://www.acmicpc.net/problem/1389
+# from collections import deque
+
+# def bfs(num,n):
+#     bacon=[0]*(n+1)
+#     visited=[num]
+#     queue=deque()
+#     queue.append(num)
+
+#     while queue:
+#         k=queue.popleft()
+#         for i in relation[k]:
+#             if i not in visited:
+#                 bacon[i]=bacon[k]+1
+#                 visited.append(i)
+#                 queue.append(i)
+
+#     return sum(bacon)
+
+
+# n,m=map(int, input().split())
+# relation={i:[] for i in range(1,n+1)}
+# for i in range(m):
+#     a,b=map(int, input().split())
+#     relation[a].append(b)
+#     relation[b].append(a)
+
+# result=[]
+# for num in range(1,n+1):
+#     result.append(bfs(num,n))
+
+# print(result.index(min(result))+1)
+
+
+# https://www.acmicpc.net/problem/10451
 from collections import deque
-
-def bfs(num,n):
-    bacon=[0]*(n+1)
-    visited=[num]
-    queue=deque()
-    queue.append(num)
-
-    while queue:
-        k=queue.popleft()
-        for i in relation[k]:
-            if i not in visited:
-                bacon[i]=bacon[k]+1
-                visited.append(i)
-                queue.append(i)
-
-    return sum(bacon)
-
-
-n,m=map(int, input().split())
-relation={i:[] for i in range(1,n+1)}
-for i in range(m):
-    a,b=map(int, input().split())
-    relation[a].append(b)
-    relation[b].append(a)
-
-result=[]
-for num in range(1,n+1):
-    result.append(bfs(num,n))
-
-print(result.index(min(result))+1)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    count = 0
+    graph = [0 for _ in range(n+1)]
+    visited = [0 for _ in range(n+1)]
+    for i,p in enumerate(list(map(int, input().split()))):
+        graph[i+1] = p
+    for i in range(1, n+1):
+        queue = deque()
+        queue.append(i)
+        visited[i] = 1
+        while queue:
+            x = queue.popleft()
+            if visited[graph[x]] == 0:
+                visited[graph[x]] = 1
+                queue.append(graph[x])
+            else:
+                if graph[x] == i:
+                    count += 1
+                    break
+    print(count)
+        
