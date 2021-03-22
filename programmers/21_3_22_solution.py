45888fba7fabee1a74894148860cc4ee951c7c9f
MinChul-Son <smc5236@naver.com>
2021-03-22 20:44:54 +0900

    알고리즘 문제풀이 : 프로그래머스 > 2018 카카오 공채 - [3차] 방금 그 곡
    문자열을 분리하는 문제로써, C와 C#과 같이 각 코드의 샵을 구분하면 쉽게
    풀 수 있는 문제이다. 재생시간을 구하고 재생된 멜로디를 만들어서 사용자가
    기억한 멜로디와 비교하면 된다.

---
+++ programmers/21_3_22_solution.py
@@ -0,0 +1,38 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17683#qna
+
+def solution(m, musicinfos):
+    answer = ['',0]
+    devide_hashTag = []
+    for c in m:
+        if c == '#':
+            devide_hashTag.append(devide_hashTag.pop() + c)
+        else:
+            devide_hashTag.append(c)
+    for i in musicinfos:
+        info_split = list(i.split(","))
+        # 재생시간 구하기
+        t1_h, t1_m = map(int, info_split[0].split(":"))
+        t2_h, t2_m = map(int, info_split[1].split(":"))
+        play_time = (t2_h-t1_h)*60 + (t2_m-t1_m)
+        melody_devide = []
+        for j in info_split[3]:
+            if j == '#':
+                melody_devide.append(melody_devide.pop() + j)
+            else:
+                melody_devide.append(j)
+        if play_time < len(melody_devide):
+                melody_devide = melody_devide[:play_time]
+        else:
+            melody_devide = melody_devide*(play_time//len(melody_devide)) + melody_devide[:(play_time%len(melody_devide))]
+        for i in range(len(melody_devide) - len(devide_hashTag) + 1):
+            if ''.join(melody_devide[i:i+len(devide_hashTag)]) == ''.join(m) and play_time > answer[1]:
+                answer = [info_split[2], play_time]
+    if answer[1]:
+        return answer[0]
+    return '(None)'
+
+
+
+
+print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
+
