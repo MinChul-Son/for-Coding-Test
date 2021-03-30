162ea40dfd8fb361519d54e6d029e1f3149df9ce
MinChul-Son <smc5236@naver.com>
2021-03-30 17:29:52 +0900

    알고리즘 공부 : 동빈북 - 최단경로 > 힙 라이브러리 사용 예제(최소힙)
    파이썬의 heapq 라이브러리를 사용하여 간단한 heapsort를 구현했다.

--- shortest_path_study_dongbinbook.py
+++ shortest_path_study_dongbinbook.py
@@ -58,3 +58,21 @@ for i in range(1, n + 1):
     # 도달할 수 있는 경우 거리를 출력
     else:
         print(distance[i])
+
+# ---------------------------------------------------------------------
+# 힙 라이브러리 사용 예제 : 최소 힙
+import heapq
+
+# 오름차순 힙 정렬(Heap Sort)
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
+result = heapsort([1,3,5,7,9,2,4,6,8,0])
+print(result)
