cabf09c52a5609b906d704d5a0e9333da4373fb6
MinChul-Son <smc5236@naver.com>
2021-04-05 17:59:01 +0900

    알고리즘 문제 풀이 : 백준 > 2075번 N번째 큰 수
    파이썬의 heapq모듈을 사용하여 최소 힙을 구현한 후, 최소 힙의 성질의
    비교하여 첫번째 원소보다 입력값이 크다면 첫번째 원소와 바꿔치기해주고
    그렇지 않다면 아무것도 하지않으면된다. 이렇게 되면 반복문이 종료되고
    힙의 첫번째 원소가 바로 N번째 수가 된다.

--- baekjoon/21/1/21_4_5.py
+++ baekjoon/21/1/21_4_5.py
@@ -1,16 +1,33 @@
 # https://www.acmicpc.net/problem/1715
+# import sys
+# import heapq
+# input = sys.stdin.readline
+# n = int(input())
+# queue = []
+# for _ in range(n):
+#     heapq.heappush(queue, int(input()))
+# answer = 0
+# while len(queue) > 1:
+#     x = heapq.heappop(queue) ; y = heapq.heappop(queue)
+#     answer += (x+y)
+#     heapq.heappush(queue, x+y)
+# print(answer)
+
+
+# https://www.acmicpc.net/problem/2075
 import sys
 import heapq
 input = sys.stdin.readline
 n = int(input())
-queue = []
-for _ in range(n):
-    heapq.heappush(queue, int(input()))
-answer = 0
-while len(queue) > 1:
-    x = heapq.heappop(queue) ; y = heapq.heappop(queue)
-    answer += (x+y)
-    heapq.heappush(queue, x+y)
-print(answer)
+queue = list(map(int, input().split()))
+heapq.heapify(queue)
+for _ in range(n-1):
+    for i in list(map(int, input().split())):
+        if i > queue[0]:
+            heapq.heappop(queue)
+            heapq.heappush(queue, i)
+print(queue[0])
+
+
 
 
