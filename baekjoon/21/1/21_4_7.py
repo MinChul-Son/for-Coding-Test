338b1c3f19484f815d35b2435f988ed2a04f156d
MinChul-Son <smc5236@naver.com>
2021-04-07 18:17:43 +0900

    알고리즘 문제 풀이 : 백준 > 2812번 크게 만들기
    스택의 성질을 사용하여 pop과 push의 과정을 반복하여 큰 수 만들어 해결

--- baekjoon/21/1/21_4_7.py
+++ baekjoon/21/1/21_4_7.py
@@ -1,18 +1,35 @@
 # https://www.acmicpc.net/problem/11000
+# import sys
+# import heapq
+# input = sys.stdin.readline
+# n = int(input())
+# lectures = [list(map(int, input().split())) for _ in range(n)]
+# lectures = sorted(lectures, key=lambda x: (x[0], x[1]))
+# count = 1
+# queue = []
+# heapq.heappush(queue, lectures[0][1])
+# for i in range(1, n):
+#     if lectures[i][0] >= queue[0]:
+#         heapq.heappop(queue)
+#         heapq.heappush(queue, lectures[i][1])
+#     else:
+#         count += 1
+#         heapq.heappush(queue, lectures[i][1])
+# print(count)
+
+
+# https://www.acmicpc.net/problem/2812
 import sys
-import heapq
 input = sys.stdin.readline
-n = int(input())
-lectures = [list(map(int, input().split())) for _ in range(n)]
-lectures = sorted(lectures, key=lambda x: (x[0], x[1]))
-count = 1
-queue = []
-heapq.heappush(queue, lectures[0][1])
-for i in range(1, n):
-    if lectures[i][0] >= queue[0]:
-        heapq.heappop(queue)
-        heapq.heappush(queue, lectures[i][1])
-    else:
-        count += 1
-        heapq.heappush(queue, lectures[i][1])
-print(count)
+n, k = map(int, input().split())
+num = list(input().strip())
+stack, temp = [], k
+for i in range(n):
+    while temp > 0 and stack and stack[-1] < num[i]:
+        del stack[-1]
+        temp -= 1
+    stack.append(num[i])
+print(''.join(stack[:n - k]))
+
+
+
