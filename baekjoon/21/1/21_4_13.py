7382bd7290c72a33eacf05ef6d3fc2f0dcdfdc2a
MinChul-Son <smc5236@naver.com>
2021-04-13 11:23:11 +0900

    알고리즘 문제 풀이 : 백준> 2294번 동전2
    dp[i] = min(dp[i-coins[0]], dp[i-coins[1]], .....) + 1
    위와같이 점화식을 세우고 문제를 해결했다.

--- baekjoon/21/1/21_4_13.py
+++ baekjoon/21/1/21_4_13.py
@@ -1,13 +1,32 @@
 # https://www.acmicpc.net/problem/1351
-from collections import defaultdict
+# from collections import defaultdict
+# import sys
+# def dp(n):
+#     if data[n] != 0:
+#         return data[n]
+#     data[n] = dp(n // p) + dp(n // q)
+#     return data[n]
+# input = sys.stdin.readline
+# n, p, q = map(int, input().split())
+# data = defaultdict(int)
+# data[0] = 1
+# print(dp(n))
+
+
+# https://www.acmicpc.net/problem/2294
 import sys
-def dp(n):
-    if data[n] != 0:
-        return data[n]
-    data[n] = dp(n // p) + dp(n // q)
-    return data[n]
 input = sys.stdin.readline
-n, p, q = map(int, input().split())
-data = defaultdict(int)
-data[0] = 1
-print(dp(n))
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [0 for _ in range(k+1)]
+dp[1] = 1
+for i in range(1, k + 1):
+    a = []
+    for j in coins:
+        if j <= i and dp[i - j] != -1:
+            a.append(dp[i - j])
+    if not a:
+        dp[i] = -1
+    else:
+        dp[i] = min(a) + 1
+print(dp[k])
