a783a367156eef8328c4e7656b536c29d555a2ca
MinChul-Son <smc5236@naver.com>
2021-04-29 19:08:44 +0900

    동빈북 : REST API 호출하여 회원 정보 처리
    jsonplaceholder의 rest api를 호출하여 가져온 회원 정보 json형식의
    데이터를 파이썬 객체로 변환하고 리스트에 담아 출력

--- For_Development_Test.py
+++ For_Development_Test.py
@@ -16,5 +16,34 @@ user = {
     "hobby": ["coding", "game"]
 }
 
-json_date = json.dumps(user, indent=4)
-print(json_date)
+json_data = json.dumps(user, indent=4)
+print(json_data)
+
+
+#------------------------------------------------------------
+import json
+
+user = {
+    "id": "minchul",
+    "password": "1234",
+    "age": 25,
+    "hobby": ["coding", "game"]
+}
+
+with open("user.json", "w", encoding="utf-8") as file:
+    json_date = json.dump(user, file, indent=4)
+
+
+#-----------------------------------------------------------------
+import requests
+
+target = "https://jsonplaceholder.typicode.com/users"
+response = requests.get(url=target)
+
+data = response.json()
+
+name_list = []
+for user in data:
+    name_list.append(user['name'])
+
+print(name_list)
