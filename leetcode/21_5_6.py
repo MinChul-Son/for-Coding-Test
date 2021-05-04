dbd83914560d09260895981555bef3395ae31ca5
MinChul-Son <smc5236@naver.com>
2021-05-04 14:34:23 +0900

    LeetCode : Remove Duplicates from Sorted Array
    인덱싱을 통하여 중복 제거

---
+++ leetcode/21_5_6.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)==0: return 0
+        
+        
+        i=0 
+        for j in range(1,len(nums)):
+            if nums[i]!=nums[j]: # If not duplicated
+                i+=1  # Move i
+                nums[i]=nums[j] # Swap
+        
+        return i+1
+
+#----------------------------------------------------------------------------------------------------
