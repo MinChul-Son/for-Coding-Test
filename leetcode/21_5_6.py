eef7af41968fa001df497ddbf3f936ea82b6b294
MinChul-Son <smc5236@naver.com>
2021-05-05 14:34:23 +0900

    LeetCode : Best Time to Buy and Sell Stock 2
    Greedy한 방식으로 문제를 해결했다.
    현재 가격보다 다음 날의 가격이 더 높으면 파는 식으로 문제 해결

--- leetcode/21_5_6.py
+++ leetcode/21_5_6.py
@@ -14,3 +14,13 @@ class Solution:
         return i+1
 
 #----------------------------------------------------------------------------------------------------
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if prices == sorted(prices, reverse=True):
+            return 0
+        total_prices = 0
+        for i in range(len(prices)-1):
+            if prices[i] < prices[i+1]:
+                total_prices += prices[i+1] - prices[i]
+        return total_prices
