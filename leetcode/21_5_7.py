59d0a3d7997302449cc3b921206b15ad9fbcded1
MinChul-Son <smc5236@naver.com>
2021-05-07 16:34:06 +0900

    LeetCode : Move Zeroes
    0개수 카운트하고 그만큼 remove&append

--- leetcode/21_5_7.py
+++ leetcode/21_5_7.py
@@ -6,3 +6,19 @@ class Solution:
         for i in plus_one:
             digits.append(int(i))
         return digits
+
+
+#-------------------------------------------------------------------------------------------
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 1:
+            pass
+        else:
+            count_zero = nums.count(0)
+            for _ in range(count_zero):
+                nums.remove(0)
+                nums.append(0)
