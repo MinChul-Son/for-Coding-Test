9fea29fc765ad76ad0a849ad9629136dbc021d0e
MinChul-Son <smc5236@naver.com>
2021-05-11 15:50:51 +0900

    알고리즘 문제 풀이 : 7662번 수정
    풀이 수정

--- baekjoon/21/1/21_5_11.py
+++ baekjoon/21/1/21_5_11.py
@@ -32,45 +32,47 @@ for _ in range(t):
     else:
         print("EMPTY")
 #-------------------------------------------------------------------------
+import sys
 import heapq
-def sync(arr):
-    while arr and id[arr[0][1]] == 0:
-        heapq.heappop(arr)
+input = sys.stdin.readline
 
-T = int(input())
-for test_case in range(T):
-    max_arr = []
-    min_arr = []
-    id = [0] * 1000000
-    K = int(input())
-    count = 0
-    for i in range(K):
-        S, num = input().split()
 
-        if S == "I":
-            heapq.heappush(max_arr, (-1 * int(num), i))
-            heapq.heappush(min_arr, (int(num),i))
-            id[i] = 1
-            
-        else:
+test = int(input())
+for _ in range(test):
+    max_heap, min_heap = [], []
+    visit = [False] * 1_000_001
 
-            if num == "1":
-                sync(max_arr)
-                if max_arr:
-                    id[max_arr[0][1]] = 0
-                    heapq.heappop(max_arr)
+    order_num = int(input())
 
-            elif num == "-1":
-                sync(min_arr)
-                if min_arr:
-                    id[min_arr[0][1]] = 0
-                    heapq.heappop(min_arr)
+    for key in range(order_num):
+        order = input().rsplit()
+        if order[0] == 'I':
+            heapq.heappush(min_heap, (int(order[1]), key))
+            heapq.heappush(max_heap, (int(order[1]) * -1, key))
+            visit[key] = True #True이면 어떤 힙에서도 아직 삭제되지 않은 상태
 
-    sync(max_arr)
-    sync(min_arr)
+        elif order[0] == 'D':
+            if order[1] == '-1': #삭제연산시, key값을 기준으로 해당 노드가 다른힙에서 삭제된 노드인가를 먼저 판단한다.
+                # 이미 상대힙에 의해 삭제된 노드인경우 삭제되지 않은 노드가 나올때까지 계쏙 버리다가 이후 삭제대상노드가 나오면 삭제한다.
+                while min_heap and not visit[min_heap[0][1]]: # visit이 False일떄 -> 해당노드가 삭제된상태
+                    heapq.heappop(min_heap) # 버림 (상대힙에서 이미 삭제된노드이므로)
+                if min_heap:
+                    visit[min_heap[0][1]] = False # visit이 Ture엿으므로 False로 바꾸고 내가 삭제함
+                    heapq.heappop(min_heap)
+            elif order[1] == '1':
+                while max_heap and not visit[max_heap[0][1]]: #이미 삭제된 노드인경우 삭제되지 않은 노드가 나올때까지 모두 버린다.
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visit[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
 
-    if len(max_arr) == 0:
-        print("EMPTY")
+# 모든 연산이 끝난후에도 ㅅ쓰레기 노드가 들어있을수 있으므로, 결과를 내기전 모두 비우고 각 힙의 첫번째 원소값을 출력한다. 
+    while min_heap and not visit[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visit[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if min_heap and max_heap:
+        print(-max_heap[0][0], min_heap[0][0])
     else:
-        print(-1 * max_arr[0][0], end =" ")
-        print(min_arr[0][0])
+        print('EMPTY')
