e7dc627fc112803b2493f78183c754af3852bb89
MinChul-Son <smc5236@naver.com>
2021-05-26 00:04:43 +0900

    LeetCode : String to Integer
    정규표현식을 사용

--- leetcode/21_5_25.py
+++ leetcode/21_5_25.py
@@ -32,3 +32,30 @@ class Solution:
             return True
         else:
             return False
+
+
+#--------------------------------------------------------------
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/884/
+import re 
+class Solution: 
+    def myAtoi(self, string) -> int: 
+        answer = 0 
+        string = string.strip() 
+        if string == "": 
+            answer = 0 
+        else: 
+            number_list = re.findall(r"^[-+]?[0-9]+", string) 
+            print(number_list) 
+            if len(number_list) == 0: 
+                answer = 0 
+            else: 
+                number = int(number_list[0]) 
+                if number > pow(2, 31) - 1: 
+                    number = pow(2, 31) -1 
+                elif number < pow(2, 31) * (-1): 
+                    number = pow(2, 31) * (-1) 
+
+                answer = number 
+        return answer
+
+
