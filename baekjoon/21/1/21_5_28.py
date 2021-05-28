8853c4691b02ad56a37d9e06227d0de6e82d6c68
MinChul-Son <smc5236@naver.com>
2021-05-28 19:04:23 +0900

    백준 : 1062번 > 가르침 (실패)
    왜 틀리는지를 모르겠음..

---
+++ baekjoon/21/1/21_5_28.py
@@ -0,0 +1,38 @@
+# https://www.acmicpc.net/problem/1062
+## 실패
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+words = []
+answer = 0
+for _ in range(N):
+    input_word = input().strip()
+    words.append(set(input_word[4:-4]))
+learned = ["a", "n", "t", "i", "c"]
+
+if K < 5:
+    print(0)
+else:
+    words = sorted(words, key=lambda x:len(x))
+    K -= 5
+    for word in words:
+        if word == "":
+            answer += 1
+            continue
+        for alpha in word:
+            if alpha in learned:
+                pass
+            else:
+                if K == 0:
+                    break
+                K -= 1
+                learned.append(alpha)
+        else:
+            answer += 1
+    print(answer)
+        
+
+#--------------------------------------------------------------------------
+
+
+
