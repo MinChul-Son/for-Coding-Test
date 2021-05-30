1893a26b6170a8153ac9bed6c38808cd40f09679
MinChul-Son <smc5236@naver.com>
2021-05-30 20:01:52 +0900

    백준 : 1806번 > 부분합
    투포인터 알고리즘을 사용하여 해결

--- baekjoon/21/1/21_5_30.py
+++ baekjoon/21/1/21_5_30.py
@@ -1,4 +1,4 @@
-# https://www.acmicpc.net/problem/1700
+# # https://www.acmicpc.net/problem/1700
 import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
@@ -27,3 +27,36 @@ for i in range(len(elec_items)):
             multi_tap.append(elec_items[i])
             answer += 1
 print(answer)
+
+#-------------------------------------------------------------------------------------------
+# https://www.acmicpc.net/problem/1806
+import sys
+
+N, S = map(int, sys.stdin.readline().split(" "))
+arr = list(map(int, sys.stdin.readline().split(" ")))
+
+start = 0
+end = 1
+result = N
+sum = arr[start]
+if sum == S:
+    result = 1
+flag = 0
+
+while not (start == end == N):
+    if sum < S and end < N:
+        sum += arr[end]
+        end += 1
+    else:
+        sum -= arr[start]
+        start += 1
+
+    if sum >= S:
+        result = min(result, end-start)
+        flag = 1
+
+if flag == 0:
+    print(0)
+else:
+    print(result)
+        
