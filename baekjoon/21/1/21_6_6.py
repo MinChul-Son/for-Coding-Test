d2a0162710b830d811ca699eedac4566bf6f3477
MinChul-Son <smc5236@naver.com>
2021-06-06 22:17:49 +0900

    백준 : 15989번 > 1,2,3 더하기4
    모든 수를 1로만 표현할 수 있으므로 기본값 1세팅
    나머지 경우의 수는 현재의 수보다 2,3 작은 수의 경우를 더해줌

--- baekjoon/21/1/21_6_6.py
+++ baekjoon/21/1/21_6_6.py
@@ -10,5 +10,19 @@ for i in range(n):
     if i + cost_list[i][0] > n:
         continue
     dp[i+cost_list[i][0]] = max(max_cost+cost_list[i][1], dp[i+ cost_list[i][0]])
-    print(dp)
 print(max(dp))
+
+#------------------------------------------------------------
+# https://www.acmicpc.net/problem/15989
+import sys
+input = sys.stdin.readline
+t = int(input())
+dp = [1 for i in range(10001)] # 1로만 표현하는 것은 무조건 존재하므로 1부터 시작
+nums = [int(input()) for _ in range(t)]
+
+for i in range(2, 10001):
+    dp[i] += dp[i - 2]
+for i in range(3, 10001):
+    dp[i] += dp[i - 3]
+for i in nums:
+    print(dp[i])
