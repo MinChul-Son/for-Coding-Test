b9ca37d65454a8c2bbadafc5fd4dbfafa2764857
MinChul-Son <smc5236@naver.com>
2021-08-21 22:29:13 +0900

    프로그래머스 : 음양 더하기
    설명가지 x. 너무 쉬움

--- programmers/21_08_21_solution.py
+++ programmers/21_08_21_solution.py
@@ -24,3 +24,47 @@ def solution(lottos, win_nums):
     return answer
 
 # https://programmers.co.kr/learn/courses/30/lessons/81301?language=python3
+def solution(s):
+    nums = {
+        'zero' : '0',
+        'one' : '1',
+        'two' : '2',
+        'three' : '3',
+        'four' : '4',
+        'five' : '5',
+        'six' : '6',
+        'seven' : '7',
+        'eight' : '8',
+        'nine' : '9'
+    }
+    for key, value in nums.items():
+        s = s.replace(key,value)
+
+    return int(s)
+
+def solution(s):
+    answer = ""
+    num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    make_num = ""
+    for i in s:
+        if i.isdecimal():
+            answer += i
+        else:
+            make_num += i
+            search_from_dic = num_dic.get(make_num)
+            if search_from_dic == None:
+                continue
+            answer += search_from_dic
+            make_num = ""
+    return int(answer)
+
+
+# https://programmers.co.kr/learn/courses/30/lessons/76501?language=python3
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
