b734e28c890168cc1d017c21632ce54393ea5d50
MinChul-Son <smc5236@naver.com>
2021-09-01 22:57:40 +0900

    백준 : 2346번 > 풍선 터뜨리기

--- baekjoon/21/1/21_9_1.py
+++ baekjoon/21/1/21_9_1.py
@@ -1,19 +1,46 @@
 # https://www.acmicpc.net/problem/10799
-import sys
+# import sys
 
-input = sys.stdin.readline().strip()
-answer = 0
-stack = []
-for i,p in enumerate(input):
-    if p == '(':
-        stack.append(p)
-        continue
+# input = sys.stdin.readline().strip()
+# answer = 0
+# stack = []
+# for i,p in enumerate(input):
+#     if p == '(':
+#         stack.append(p)
+#         continue
     
-    stack.pop()
+#     stack.pop()
 
-    if input[i-1] == '(':
-        answer += len(stack)
+#     if input[i-1] == '(':
+#         answer += len(stack)
+#     else:
+#         answer += 1
+
+# print(answer)
+
+# https://www.acmicpc.net/problem/2346
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+balloons = list(map(int, input().split()))
+answer = [1]
+
+for i in range(len(balloons)):
+    balloons[i] = (balloons[i], i+1)
+
+move = balloons[0][0]
+position = 0
+balloons.pop(0)
+
+for _ in range(N-1):
+    if move < 0:
+        position = (position + move) % len(balloons)
     else:
-        answer += 1
+        position = (position + move - 1) % len(balloons)
+    
+    temp = balloons.pop(position)
+    move = temp[0]
+    answer.append(temp[1])
 
-print(answer)
+print(*answer)
