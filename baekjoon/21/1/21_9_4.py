fb7f2dd44b004f731cab838a50ddea6cc5f00108
MinChul-Son <smc5236@naver.com>
2021-09-04 13:23:21 +0900

    백준 : 1343번> 폴리오미노
    replace 사용

--- baekjoon/21/1/21_9_4.py
+++ baekjoon/21/1/21_9_4.py
@@ -11,3 +11,16 @@ elif rest % 2 == 0:
     print(n // 5 + rest // 2)
 else:
     print((n // 5) -1 + (rest + 5) // 2)
+
+# https://www.acmicpc.net/problem/1343
+import sys
+input = sys.stdin.readline
+
+board = input().strip()
+board = board.replace("XXXX", "AAAA") 
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+else:
+    print(board)
