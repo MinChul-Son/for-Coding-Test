092bd764ac5fc5cf4e2b7388877957478cfcac90
MinChul-Son <smc5236@naver.com>
2021-10-30 15:17:38 +0900

    코테 : NHN
    - NHN : 3솔
    - NHN 커머스 : 2솔

---
+++ java/src/CodingTest1.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CodingTest1 {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+//        int n = scanner.nextInt(); // 송수신 기록의 수
+//        String[] logs = new String[n]; // 송수신 기록
+//        for(int i = 0 ; i < n ; i ++)
+//        {
+//            logs[i] = scanner.nextLine().trim();
+//        }
+//
+//        List<String> record = new ArrayList<>();
+//        record.add(logs[0]);
+//
+//        for (int i = 1; i < n; i++) {
+//            System.out.println(i);
+//            if (logs[i - 1].equals(logs[i])){
+//                String s = record.get(record.size() - 1);
+//                if (s.substring(s.length() - 1).equals(")")) {
+//                    int time = Integer.parseInt(s.substring(s.length() - 2, s.length() - 1));
+//                    record.set(i - 1, logs[i] + " (" + String.valueOf(time) + ")");
+//                } else {
+//                    record.set(i - 1, logs[i] + " (2)");
+//                }
+//                continue;
+//            }
+//            record.add(logs[i]);
+//        }
+
+        Map<Integer, String> phoneNumbers = new HashMap<>();
+
+
+    }
+}
---
+++ java/src/Solution1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        TreeMap<Integer, Integer> tree = new TreeMap<Integer, Integer>();
+        tree.put(1, 1);
+        List<Integer> collect = tree.values().stream().sorted().collect(Collectors.toList());
+//        Object[] objects = tree.keySet().toArray();
+//        Integer[] integers = Arrays.copyOf(objects, objects.length, Integer[].class);
+//        tree.put(integers[0], integers[0] + 1);
+//        List<Integer> list = new ArrayList<>();
+        int[] arr = {1,2,3,4};
+
+    }
+}
